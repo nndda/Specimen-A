f8216bfbc9763bfe69d9014140b76a4019d8ebe3
nnda <96333146+nndda@users.noreply.github.com>
2023-02-05 04:49:07 +0800

    Smoothen player's movement

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -2,6 +2,7 @@ extends Node
 
 var allow_move : bool = true
 var moving : bool
+var moving_f : float
 
 var worm_segment_max : int = 36
 var worm_length : float
@@ -15,6 +16,14 @@ func sum_array(array:PoolRealArray) -> float:
 		 t += n
 	return t
 
-func _process(_delta):
+func _process(delta):
+
 	if allow_move:
 		moving = true if Input.is_action_pressed("Move") else false
+
+	if moving:
+		moving_f += 0.1
+	else:
+		moving_f -= 0.085
+#	moving_f += (1.1 if moving else -2.0 * 0.05) * delta
+	moving_f = clamp(moving_f,0.0,1.0)
