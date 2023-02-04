b90cc8eefbb00b1f91769020b023787fc245d26f
nnda <96333146+nndda@users.noreply.github.com>
2023-02-05 04:49:24 +0800

    Decorate player's chr

--- Player/Player.Body.gd
+++ Player/Player.Body.gd
@@ -13,11 +13,24 @@ func UpdateWormLength() -> void:
 				points[sgmnt].distance_to( points[ wrapi( sgmnt, 0, sgmnt_count ) + 1 ] )
 			)
 
+
+func UpdateLightPath() -> void:
+	for n in get_point_count() - 1 :
+		$Lights.curve.set_point_position(
+			(get_point_count() - 2) - n,
+			points[n] )
+
+func _ready():
+	$Lights.curve.clear_points()
+	for n in glbl.worm_segment_max:
+		$Lights.curve.add_point(Vector2(0,0))
+
 func _process(_delta):
 	if get_point_count() > glbl.worm_segment_max:
-		if glbl.moving:
+		if glbl.moving_f > 0:
 			remove_point(0)
 	else:
-		if glbl.moving:
+		if glbl.moving_f > 0:
 			add_point($"../Head".position)
 	UpdateWormLength()
+	UpdateLightPath()
--- Player/Player.Head.gd
+++ Player/Player.Head.gd
@@ -7,14 +7,16 @@ var speed = 430.0
 var velo
 func _physics_process(_delta):
 
+	$"../CanvasLayer/Label".text = str(glbl.moving_f)
+
 	easer += ( 0.1 * 1.0 if glbl.moving else -1.0 )
 	easer = clamp(easer,0.0,1.0)
 
 	velo = move_and_slide(
-		float(glbl.moving) * ( ease(easer,0.5) * speed ) * global_position.direction_to(get_global_mouse_position())
+		float(glbl.moving_f) * (( speed ) * global_position.direction_to(get_global_mouse_position()))
 	)
 
-	if glbl.moving:
+	if glbl.moving_f > 0:
 		self.look_at(get_global_mouse_position())
 
 	if Input.is_action_pressed("ui_accept"):
