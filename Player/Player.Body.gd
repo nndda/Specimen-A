2abd3be10ef173c70a56d8ccce2eea81e24693be
nnda <96333146+nndda@users.noreply.github.com>
2023-02-06 03:10:53 +0800

    Toggle health bar UI for performance

--- Player/Player.Body.gd
+++ Player/Player.Body.gd
@@ -19,12 +19,38 @@ func UpdateLightPath() -> void:
 			(get_point_count() - 2) - n,
 			points[n] )
 
+var health_bar_offsets : PoolRealArray = PoolRealArray([
+	0.02,0.04 ])
+onready var health_bar = $"../HealthUI"
 func UpdateHealthUI() -> void:
-	for n in get_point_count() - 1:
-		if $"../HealthUI".get_point_count() <= n:
-			$"../HealthUI".add_point(points[n])
-		else:
-			$"../HealthUI".points[n] = points[n]
+	if health_bar.visible:
+		health_bar_offsets[0] = range_lerp(glbl.health,100.0,0.0,0.02,0.96)
+		health_bar_offsets[1] = range_lerp(glbl.health,100.0,0.0,0.04,0.98)
+
+		health_bar_offsets[0] = clamp(health_bar_offsets[0],0.02,0.96)
+		health_bar_offsets[1] = clamp(health_bar_offsets[1],0.04,0.98)
+
+		for n in ( get_point_count() - 1 ):
+			if health_bar.get_point_count() <= n:
+				health_bar.add_point(points[n])
+			else:
+				health_bar.points[n] = points[n]
+
+		health_bar.gradient.offsets[1] = health_bar_offsets[0]
+		health_bar.gradient.offsets[2] = health_bar_offsets[1]
+
+#	health_bar.width_curve.set_point_value(1,
+#		range_lerp(glbl.health,0,100,0.0,0.5)
+#		)
+#	health_bar.width_curve.set_point_value(2,
+#		range_lerp(glbl.health,0,100,0.0,0.75)
+#		)
+#	health_bar.width_curve.set_point_value(3,
+#		range_lerp(glbl.health,0,100,0.0,1.0)
+#		)
+#	health_bar.width_curve.set_point_value(4,
+#		range_lerp(glbl.health,0,100,0.0,1.0)
+#		)
 
 #func ri() -> String:
 #	return str(randi() % 9)
