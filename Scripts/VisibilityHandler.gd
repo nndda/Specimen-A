923e8e7321b2e58bfea06e58f01a5390130054c5
nnda <96333146+nndda@users.noreply.github.com>
2023-02-06 03:11:31 +0800

    Create visibility handler for performance

---
+++ Scripts/VisibilityHandler.gd
@@ -0,0 +1,14 @@
+extends VisibilityNotifier2D
+
+func _enter_tree():
+	self.show()
+func _ready():
+	var err = connect( "ready", get_parent(), "hide" )
+	if err != 0:
+		push_warning(" > on : " + str( self ) + ", " + str(err))
+#	print( " > on : " + str( self ) + ", " + str(err) )
+
+func _on_VisibilityHandler_screen_entered():
+	get_parent().show()
+func _on_VisibilityHandler_screen_exited():
+	get_parent().hide()
