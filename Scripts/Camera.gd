77546d000863c821935565a81abc8e65ba4711a8
nnda <96333146+nndda@users.noreply.github.com>
2023-02-06 03:11:10 +0800

    Dynamic camera movement

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -2,3 +2,5 @@ extends Camera2D
 
 func _process(_delta):
 	global_position = glbl.head_pos
+	offset_h = clamp( (get_global_mouse_position().x - self.global_position.x) / 174 , -1 , 1 )
+	offset_v = clamp( (get_global_mouse_position().y - self.global_position.y) / 125 , -1 , 1 )
