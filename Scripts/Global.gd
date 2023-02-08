8270f3549a95b7ec23eebbbff137f82f42da73b8
nnda <96333146+nndda@users.noreply.github.com>
2023-02-08 09:46:38 +0800

    Update global singleton

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,14 +1,17 @@
-extends Node
+extends Node2D
 
-var top_scale = 1.2
+var top_scale = 1.1
+
+var camera : Object
 
 var allow_move : bool = true
 var moving : bool
 var moving_f : float
 
 var head_pos : Vector2
+var head_canvas_pos : Vector2
 
-var worm_segment_max : int = 32
+var worm_segment_max : int = 40
 var worm_length : float
 var worm_length_array : Array
 
@@ -21,6 +24,28 @@ func sum_array(array:PoolRealArray) -> float:
 	for n in array:
 		 t += n
 	return t
+
+
+var attack_mode : int = 0
+func _input(event : InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			match event.button_index:
+				BUTTON_WHEEL_UP:
+					attack_mode += 1
+				BUTTON_WHEEL_DOWN:
+					attack_mode -= 1
+#		print(attack_mode)
+
+#	CONFIG
+var cfg = {
+	"always_show_health_bar"	: false,
+	"optimal_graphic"			: false,
+}
+
+
+
+
 
 func _process(_delta):
 
@@ -40,5 +65,7 @@ func _process(_delta):
 		moving_f += 0.1
 	else:
 		moving_f -= 0.085
-#	moving_f += (1.1 if moving else -2.0 * 0.05) * delta
-	moving_f = clamp(moving_f,0.0,1.0)
+
+	moving_f = clamp( moving_f, 0.0,
+		float(glbl.head_pos.distance_to(get_global_mouse_position()) >= 25)
+	)
