9bfc7a27fa7e78c00bab256b2a7867bb8ee83560
nnda <96333146+nndda@users.noreply.github.com>
2023-02-08 09:47:34 +0800

    Update level 01

--- Levels/01/01.gd
+++ Levels/01/01.gd
@@ -5,5 +5,6 @@ func _ready():
 #	pass
 	$"Tile-Wall".occluder_light_mask = 0
 	$"Tile-Wall_3D".GenerateLayers()
-	$PostFX.show()
+#	$PostFX.show()
+	$TopLayer.follow_viewport_scale = glbl.top_scale
 	
