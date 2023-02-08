bad06597b11dd0afb17e08f4fe76c00452449ad6
nnda <96333146+nndda@users.noreply.github.com>
2023-02-08 09:44:15 +0800

    Update LightBehaviourHadler

--- Shaders/Lights/LightsBehaviourHandler.gd
+++ Shaders/Lights/LightsBehaviourHandler.gd
@@ -2,19 +2,27 @@ extends Area2D
 
 # Put directly below light's parent
 
+export(NodePath) var light
+
 func _ready():
-#	$AnimationPlayer.root_node = $AnimationPlayer.get_path_to(get_parent())
 	$Timer.start(rand_range(120.0,600.0))
-#	$Timer.start(rand_range(2.0,6.0))
 
+func _process(_delta):
+	get_node(light).shadow_enabled = !glbl.cfg.optimal_graphic
 
-func _on_self_area_entered(_area):
-	pass # Replace with function body.
+func _on_self_area_entered(area):
+	if area.get_name() == "AreaShake":
+		Flick()
 func _on_self_body_entered(_body):
 	pass # Replace with function body.
 
+func Flick() -> void:
+	$Timer.start(rand_range(120.0,600.0))
+	if randf() > 0.5:
+		$AnimationPlayer.play("Flick")
+	else:
+		$AnimationPlayer.play("Flick2")
 
 func _on_Timer_timeout():
 	$Timer.start(rand_range(120.0,600.0))
-#	$Timer.start(rand_range(4.0,6.0))
-	$AnimationPlayer.play("Flick")
+	Flick()
