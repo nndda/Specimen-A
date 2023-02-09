f287004afdf186cdda4d8014ec105530bbca5560
nnda <96333146+nndda@users.noreply.github.com>
2023-02-10 03:40:57 +0800

    Update base script for rifle

---
+++ Entities/Guards/Gun-Rifle.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+func _ready():
+	$LineOfSight.add_exception($"..")
+	$LineOfSight.add_exception($"../TriggerArea")
+
+	$LineOfSight/LineOfFire.add_exception($"..")
+	$LineOfSight/LineOfFire.add_exception($"../TriggerArea")
+
+func _physics_process(_delta):
+	$BulletPath.visible = get_parent().firing
+	$BulletPath.points[0] = Vector2(0,0)
+	$BulletSpark.visible = $LineOfSight/LineOfFire.is_colliding()
+
+	if $LineOfSight/LineOfFire.is_colliding():
+		$CollidingPoint.global_position = $LineOfSight/LineOfFire.get_collision_point()
+		$BulletPath.points[1] = $CollidingPoint.position# - Vector2(14,5)
+
+		$BulletSpark.global_position = $CollidingPoint.global_position
+		if $LineOfSight/LineOfFire.get_collider().has_method("DamagePlayer"):
+			$LineOfSight/LineOfFire.get_collider().DamagePlayer(12.0)
+	else:
+		$BulletPath.points[1] = Vector2(0,110)
+
+func Fire() -> void:
+#	$BulletSpark.emitting = true
+	randomize()
+	$LineOfSight/LineOfFire.rotation_degrees = rand_range(-2.25,2.25)
+	$BulletPath.rotation_degrees = $LineOfSight/LineOfFire.rotation_degrees
+
+
