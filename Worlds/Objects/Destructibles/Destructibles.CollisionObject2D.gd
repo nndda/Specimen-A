d96d6e592193c6650b494e4b41600b894af96c97
nnda <96333146+nndda@users.noreply.github.com>
2023-02-10 03:44:19 +0800

    Update Destructibles.CollisionObject2D.gd

--- Worlds/Objects/Destructibles/Destructibles.CollisionObject2D.gd
+++ Worlds/Objects/Destructibles/Destructibles.CollisionObject2D.gd
@@ -6,7 +6,15 @@ export(NodePath) var particle_node
 
 export(Array) var destroy_objects
 
+var max_health
+
+export(int) var particle_amount = 30
+
+var particle_max : int = 30
+
 func _ready():
+	max_health = health
+	particle_max = particle_amount
 	if get_node(particle_node) != null:
 		get_node(particle_node).hide()
 
@@ -15,6 +23,12 @@ func Damage(power:float = 35.0) -> void:
 
 	if get_node(particle_node)	!= null:
 		var particle			= get_node(particle_node).duplicate()
+		particle.amount			= int(clamp( particle_amount - (particle_max - particle_amount) * (health/max_health),1,particle_max))
+		particle_amount			-= particle.amount
+		particle_amount			= clamp(particle.amount,0,particle_max)
+		print(particle_amount)
+		print(particle.amount)
+#		particle.amount			= int( ( 30 * ( max_health - health ) ) / max_health )
 		particle.visible		= true
 		particle.emitting		= true
 		get_parent().add_child(particle)
