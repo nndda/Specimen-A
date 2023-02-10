960b4f27c86e326c81cdfa2f7653074d32dfc06f
nnda <96333146+nndda@users.noreply.github.com>
2023-02-11 03:09:16 +0800

    Create navigation system

--- Entities/Guards/Guards.gd
+++ Entities/Guards/Guards.gd
@@ -3,28 +3,39 @@ extends KinematicBody2D
 var health = 80
 var dead = false
 
+var speed = 60.0
+
 var triggered = false
 var firing = false
 
 onready var line_of_sight = $Gun/LineOfSight
 var fire_clear = false
-
 onready var line_of_fire = $Gun/LineOfSight/LineOfFire
 var fire_ready = false
 
-export(float,0.0,5.0) var init_cooldown = 0
+export(float,0.0,5.0) var init_cooldown = 0.0
 var clear = true
 export(float,0.8,5.0) var cooldown
 
 export(String) var group
 
+signal path_changed(path)
+signal target_reached
+onready var nav_agent = $NavigationAgent2D
+
+var velocity		: Vector2
+var move_direction	: Vector2
+
+
+func GetPlayerPos() -> Vector2:
+	return glbl.head_pos
+
+
 func _ready():
 	clear = init_cooldown == 0
-	if group != "":
-		add_to_group(group)
-	$GlobalModulate.queue_free()
-	$ColorRect.queue_free()
 	$AttackCooldown.wait_time = cooldown
+
+
 
 var ot = true
 func _process(_delta):
@@ -35,7 +46,6 @@ func _process(_delta):
 					group_n.triggered = true
 				ot = false
 		rotation_degrees = wrapf(rotation_degrees,0,360)
-		look_at(glbl.head_pos)
 	
 	firing = $Gun/AnimationPlayer.is_playing()
 	line_of_fire.enabled = firing
@@ -44,42 +54,75 @@ func _process(_delta):
 		$Gun/AnimationPlayer.play("Firing")
 		fire_ready = false
 
+func _physics_process(_delta):
+	
+	var move_direction = position.direction_to(nav_agent.get_next_location())
+	velocity = move_direction * speed
+	look_at_direction(move_direction)
+	look_at(glbl.head_pos)
+
+	nav_agent.set_velocity(velocity)
+
 
 func Weapon_AnimationFinished(anim):
 	if anim == "Firing":
-#		ResetFire()
 		$AttackCooldown.start(cooldown)
 func _on_AttackCooldown_timeout():
 	fire_ready = true
-#	pass
-
-
-#func ResetFire() -> void:
-#	pass
-#	line_of_fire.enabled = false
 
 
 
 func Damage(power:float) -> void:
-	print("Damaged")
-	var blood = load("res://Shaders/Particles/BloodSplatter.CPUParticles2D.tscn").instance()
-	blood.custom_init_pos = true
-	blood.init_pos = global_position
+
+	var blood					= load(
+		"res://Shaders/Particles/BloodSplatter.CPUParticles2D.tscn").instance()
+	blood.custom_init_pos		= true
+	blood.init_pos				= global_position
 	get_node("../../Objects").add_child(blood)
-#	blood.global_position = global_position
-	
-	health -= power
-	
-	if health <= 0:
-		dead = true
+
+	health		-= power
+	if health	<= 0:
+		var corpse = load("res://Entities/Guards/Guard-Corpse.tscn").instance()
+		corpse.init_pos = global_position
+		get_node("../../Objects").add_child(corpse)
+		queue_free()
+		dead 	= true
 
 
+var arrived = false
+func SetTargetPos(pos:Vector2) -> void:
+	arrived = false
+	nav_agent.set_target_location(pos)
+
+func look_at_direction(direction:Vector2) -> void:
+	direction = direction.normalized()
+	move_direction = direction
+
+func _on_NavigationAgent2D_path_changed():
+	emit_signal("path_changed", nav_agent.get_nav_path())
+
+func _on_NavigationAgent2D_velocity_computed(safe_velocity):
+	if not nav_agent.is_navigation_finished():
+		velocity = move_and_slide(safe_velocity)
+	elif not arrived:
+		arrived = true
+		emit_signal("path_changed", [])
+		emit_signal("target_reached")
+
+func _on_UpdatePlayerPos_timeout():
+	if global_position.distance_to(glbl.head_pos) >= 32.0 * 5.0:
+		SetTargetPos(glbl.head_pos)
+	else:
+		$KeepDistance.rotation_degrees = rand_range(-15,15)
+		SetTargetPos($KeepDistance/Position2D.global_position)
 
 func _on_TriggerArea_body_entered(body):
 	if body.get_name() == "DamageCollision" or body.get_name() == "Head":
 		triggered = true
 		$AttackCooldown.start(cooldown)
-		print("> on: " + str(self) + " is: " + str(triggered))
+#		print("> on: " + str(self) + " is: " + str(triggered))
 		$TriggerArea.queue_free()
+
+
 
 
