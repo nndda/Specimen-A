021d36b9a7bf3b04bc9d18e561f7225fde36be1c
nnda <96333146+nndda@users.noreply.github.com>
2023-02-11 03:09:50 +0800

    Create entity turret

---
+++ Entities/Turrets/Turret.gd
@@ -0,0 +1,80 @@
+extends StaticBody2D
+
+var health = 80
+var dead = false
+
+var triggered = false
+var firing = false
+
+onready var line_of_sight = $Gun/LineOfSight
+var fire_clear = false
+onready var line_of_fire = $Gun/LineOfSight/LineOfFire
+var fire_ready = false
+
+export(float,0.0,5.0) var init_cooldown = 0.0
+var clear = true
+export(float,0.8,5.0) var cooldown
+
+export(String) var group
+
+
+func _ready():
+	clear = init_cooldown == 0
+	$AttackCooldown.wait_time = cooldown
+
+
+
+var ot = true
+func _process(_delta):
+	if triggered:
+		if ot:
+			if is_in_group(group):
+				for group_n in get_tree().get_nodes_in_group(group):
+					group_n.triggered = true
+				ot = false
+		rotation_degrees = wrapf(rotation_degrees,0,360)
+	
+	firing = $Gun/AnimationPlayer.is_playing()
+	line_of_fire.enabled = firing
+	fire_clear = triggered and fire_ready and $Gun/LineOfSight.is_colliding()
+	if fire_clear:
+		$Gun/AnimationPlayer.play("Firing")
+		fire_ready = false
+
+func _physics_process(_delta):
+	$Gun.look_at(glbl.head_pos)
+
+func Weapon_AnimationFinished(anim):
+	if anim == "Firing":
+		$AttackCooldown.start(cooldown)
+func _on_AttackCooldown_timeout():
+	fire_ready = true
+
+
+
+func Damage(power:float) -> void:
+#	var blood					= load(
+#		"res://Shaders/Particles/BloodSplatter.CPUParticles2D.tscn").instance()
+#	blood.custom_init_pos		= true
+#	blood.init_pos				= global_position
+#	get_node("../../Objects").add_child(blood)
+
+	health		-= power
+	if health	<= 0:
+#		var corpse = load("res://Entities/Guards/Guard-Corpse.tscn").instance()
+#		corpse.init_pos = global_position
+#		get_node("../../Objects").add_child(corpse)
+		queue_free()
+		dead 	= true
+
+
+func _on_TriggerArea_body_entered(body):
+	if body.get_name() == "DamageCollision" or body.get_name() == "Head":
+		triggered = true
+		$AttackCooldown.start(cooldown)
+#		print("> on: " + str(self) + " is: " + str(triggered))
+		$TriggerArea.queue_free()
+
+
+
+
