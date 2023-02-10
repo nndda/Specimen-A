c320ecabbf0a959d4a5b6a22c3782d6e2d80ac49
nnda <96333146+nndda@users.noreply.github.com>
2023-02-11 03:10:12 +0800

    Update level 1

--- Levels/01/01.gd
+++ Levels/01/01.gd
@@ -7,6 +7,12 @@ func _ready():
 	$"Tile-Wall_3D".GenerateLayers()
 #	$PostFX.show()
 	$TopLayer.follow_viewport_scale = glbl.top_scale
+	RemoveEditorItems()
 	
-func _process(_delta):
-	$Entities/Label.text = str($"Player/Head".invincible)
+#func _process(_delta):
+#	$Entities/Label.text = str($"Player/Head".invincible)
+
+func RemoveEditorItems() -> void:
+	if get_tree().has_group("DBG"):
+		for dbg in get_tree().get_nodes_in_group("DBG"):
+			dbg.queue_free()
