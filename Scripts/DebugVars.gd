5c6f5783465c33bae37d67de1dceda921d246a42
nnda <96333146+nndda@users.noreply.github.com>
2023-02-27 17:40:36 +0800

    Script to display variables

---
+++ Scripts/DebugVars.gd
@@ -0,0 +1,40 @@
+extends VBoxContainer
+
+export(Array) var data
+
+func _enter_tree():
+	if data.size() % 2 != 0:
+		push_error("\"data\" is not even")
+
+func _process(_delta):
+	set_vars()
+
+func set_vars() -> void:
+
+	if get_child_count() < data.size() / 2:
+		for varn in data.size() / 2:
+
+			var container = HBoxContainer.new()
+			var label = Label.new()
+			var value = Label.new()
+
+			label.size_flags_horizontal = 3
+
+			label.text = str( data[ varn * 2 ] )
+			value.text = str( data[ varn * 2 + 1 ] )
+
+			container.call_deferred("add_child",label)
+			container.call_deferred("add_child",value)
+
+			add_child( container )
+
+	elif get_child_count() == data.size() / 2:
+		mon_vars()
+
+
+func mon_vars() -> void:
+
+	for chd in get_children():
+
+		chd.get_child(0).text = str( data[ chd.get_position_in_parent() * 2 ] )
+		chd.get_child(1).text = str( data[ chd.get_position_in_parent() * 2 + 1 ] )
