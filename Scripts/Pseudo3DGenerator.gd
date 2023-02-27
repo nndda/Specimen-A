0c4e3174388ec14bd4ca5258325eea20a86ad2e7
nnda <96333146+nndda@users.noreply.github.com>
2023-02-27 17:43:33 +0800

    Add depth for lights tilemap

--- Scripts/Pseudo3DGenerator.gd
+++ Scripts/Pseudo3DGenerator.gd
@@ -1,7 +1,9 @@
 extends Node
 
 export(NodePath)	var map
-export(int, 8)		var depth
+export(NodePath)	var map_decor
+export(NodePath)	var map_decor_light
+export(int, 8)		var depth	 # 5
 
 onready var max_scale = glbl.top_scale
 
@@ -19,7 +21,31 @@ func GenerateLayers() -> void:
 		map_layer.navigation_layers		= 0
 		map_layer.occluder_light_mask	= 0# if n < depth - 1 else 1
 
+
 		layer.call_deferred( "add_child", map_layer)
 		layer.call_deferred( "add_child", load("res://Worlds/GlobalModulate.tscn").instance())
+
+		if n < depth - 2:
+
+			var map_decor_layer					= get_node(map_decor).duplicate()
+			map_decor_layer.collision_layer		= get_node(map_decor).collision_layer if n == 0 else 0
+			map_decor_layer.collision_mask		= get_node(map_decor).collision_mask if n == 0 else 0
+			map_decor_layer.navigation_layers	= 0
+			map_decor_layer.occluder_light_mask	= 0# if n < depth - 1 else 1
+
+			layer.call_deferred( "add_child", map_decor_layer)
+
+			if n == depth - 3:
+				var map_decor_light_layer					= get_node(map_decor_light).duplicate()
+				map_decor_light_layer.collision_layer		= get_node(map_decor_light).collision_layer if n == 0 else 0
+				map_decor_light_layer.collision_mask		= get_node(map_decor_light).collision_mask if n == 0 else 0
+				map_decor_light_layer.navigation_layers		= 0
+				map_decor_light_layer.occluder_light_mask	= 0# if n < depth - 1 else 1
+				layer.call_deferred( "add_child", map_decor_light_layer)
+#				map_decor_layer.material		= load("res://Shaders/Materials/Add.CanvasItemMaterial.tres")
+
+
 		call_deferred( "add_child", layer)
 	get_node(map).queue_free()
+#	get_node(map_decor).queue_free()
+	get_node(map_decor_light).queue_free()
