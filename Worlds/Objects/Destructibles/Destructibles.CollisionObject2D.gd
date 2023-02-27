6b0f08eed8134718d7ef34093479346b0ecba95f
nnda <96333146+nndda@users.noreply.github.com>
2023-02-27 17:49:59 +0800

    Limit damage particles and add item drop func

--- Worlds/Objects/Destructibles/Destructibles.CollisionObject2D.gd
+++ Worlds/Objects/Destructibles/Destructibles.CollisionObject2D.gd
@@ -1,4 +1,13 @@
-extends StaticBody2D
+extends CollisionObject2D
+
+#enum YieldItem {
+#	none,
+#	MetalScraps,
+#	Battery,
+#	Acids
+#}
+#export(YieldItem) var yield_item = YieldItem.MetalScraps
+#export(float,0,1) var yield_chance = 0.5
 
 export(float) var health = 100.0
 
@@ -11,6 +20,7 @@ var max_health
 export(int) var particle_amount = 30
 
 var particle_max : int = 30
+onready var particle_layer = Node2D.new()
 
 export(NodePath) var navigation
 
@@ -23,22 +33,37 @@ func _ready():
 	if get_node(particle_node) != null:
 		get_node(particle_node).hide()
 
+	get_parent().call_deferred( "add_child", particle_layer )
+
 func Damage(power:float = 35.0) -> void:
 	health -= power
+
+	glbl.PopUpPoints(power,global_position)
+
+#	match yield_item:
+#		YieldItem.MetalScraps:
+#			glbl.shrapnel_current += 1
+#		YieldItem.Battery:
+#			glbl.emp_charge += 1
+#			$ElectricityParticle.emitting = true
 
 	if get_node(particle_node)	!= null:
 		var particle			= get_node(particle_node).duplicate()
 		particle.amount			= int(clamp( particle_amount - (particle_max - particle_amount) * (health/max_health),1,particle_max))
 		particle_amount			-= particle.amount
 		particle_amount			= clamp(particle.amount,0,particle_max)
-		print(particle_amount)
-		print(particle.amount)
-#		particle.amount			= int( ( 30 * ( max_health - health ) ) / max_health )
+
+		if particle.custom_init_pos:
+			particle.init_pos = global_position
+
 		particle.visible		= true
 		particle.emitting		= true
-		get_parent().add_child(particle)
 
-	print(" Damage():" + str(power) + " -> " + str(self) )
+		if particle_layer.get_child_count() > 3:
+			particle_layer.get_child(0).queue_free()
+		particle_layer.add_child(particle)
+
+#	print(" Damage():" + str(power) + " -> " + str(self) )
 
 	if health <= 0:
 
@@ -49,4 +74,5 @@ func Damage(power:float = 35.0) -> void:
 		if get_node_or_null(navigation) != null:
 			print("nav enabled : " + str(get_node(navigation).enabled))
 			get_node(navigation).enabled = true
+
 		queue_free()
