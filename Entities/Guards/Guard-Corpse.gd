fad2fb99e4a8a6a854da2ca781aa4e13394a2424
nnda <96333146+nndda@users.noreply.github.com>
2023-02-27 17:31:04 +0800

    Add variation

--- Entities/Guards/Guard-Corpse.gd
+++ Entities/Guards/Guard-Corpse.gd
@@ -16,15 +16,23 @@ func _ready():
 
 	$Torso/Head.rotation_degrees = rand_range(-30,30)
 
-	$Torso/RArm.rotation_degrees		= rand_range(-90,30)
+#	$Torso/RArm.rotation_degrees		= rand_range(-90,30)
+	$Torso/RArm.rotation_degrees		= rand_range(-75,-30)
 	$Torso/RArm/Hand.rotation_degrees	= rand_range(0,180)
 	$Torso/LArm.rotation_degrees		= rand_range(-90,30)
 	$Torso/LArm/Hand.rotation_degrees	= rand_range(0,180)
 
-	$Torso/RLeg.rotation_degrees		= rand_range(-15,30)
+#	$Torso/RLeg.rotation_degrees		= rand_range(-15,30)
+	$Torso/RLeg.rotation_degrees		= rand_range(30,75)
 	$Torso/RLeg/Feet.rotation_degrees	= rand_range(0,-60)
 	$Torso/LLeg.rotation_degrees		= rand_range(-15,30)
 	$Torso/LLeg/Feet.rotation_degrees	= rand_range(0,-60)
+
+	if randf() >= 0.8:
+		$Torso.region_rect = Rect2(96.0,320.0,32.0,32.0)
+	else:
+		$Torso.region_rect = Rect2(128.0,320.0,32.0,32.0)
+
 
 	DecapLimb("RArm")
 	DecapLimb("LArm")
