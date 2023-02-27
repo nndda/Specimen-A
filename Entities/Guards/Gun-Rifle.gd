db2623af82f35d8ec47041c3538bb4a1afb76f4b
nnda <96333146+nndda@users.noreply.github.com>
2023-02-27 17:33:34 +0800

    Add "on_line" flag variable

--- Entities/Guards/Gun-Rifle.gd
+++ Entities/Guards/Gun-Rifle.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+var damage = 8.25
+
+onready var on_line = false
+
 func _ready():
 	$LineOfSight.add_exception($"..")
 	$LineOfSight.add_exception($"../TriggerArea")
@@ -12,15 +16,31 @@ func _physics_process(_delta):
 	$BulletPath.points[0] = Vector2(0,0)
 	$BulletSpark.visible = $LineOfSight/LineOfFire.is_colliding()
 
+	$LineOfSight/LineOfFire.enabled = $AnimationPlayer.is_playing()
+
 	if $LineOfSight/LineOfFire.is_colliding():
 		$CollidingPoint.global_position = $LineOfSight/LineOfFire.get_collision_point()
 		$BulletPath.points[1] = $CollidingPoint.position# - Vector2(14,5)
 
 		$BulletSpark.global_position = $CollidingPoint.global_position
 		if $LineOfSight/LineOfFire.get_collider().has_method("DamagePlayer"):
-			$LineOfSight/LineOfFire.get_collider().DamagePlayer(15.0)
+			$LineOfSight/LineOfFire.get_collider().DamagePlayer(damage)
+
+#			if $LineOfSight/LineOfFire.get_collider().get_name() == "DamageCollision":
+#				glbl.PopUpPoints(
+#					( damage * 0.08 )
+#					,$LineOfSight/LineOfFire.get_collision_point())
+#			else:
+#				glbl.PopUpPoints(
+#					( damage * 1.25 )
+#					,$LineOfSight/LineOfFire.get_collision_point())
+
 	else:
 		$BulletPath.points[1] = Vector2(0,110)
+		if get_node_or_null("CollidingPointDefault") != null:
+			$BulletPath.points[1] = $CollidingPointDefault.position
+
+	on_line = $LineOfSight.is_colliding()
 
 func Fire() -> void:
 #	$BulletSpark.emitting = true
