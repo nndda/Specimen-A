05d73a648f51cfcad95d25c8df1e7194a2d6dbb8
nnda <96333146+nndda@users.noreply.github.com>
2023-03-11 02:36:18 +0800

    Update LevelHandler.gd

--- Levels/LevelHandler.gd
+++ Levels/LevelHandler.gd
@@ -1,85 +1,32 @@
 extends Node2D
 
+@export_node_path("Node2D") var pseudo_3d_generator
+@export_node_path("LightOccluder2D") var global_occluder
+@export_node_path("StaticBody2D") var global_wall
+
 func _enter_tree():
 	glbl.current_scene = self
 
+
 func _ready():
-	add_child(
-		preload(
-			"res://Worlds/GlobalModulate.tscn"
-			).instantiate()
-	)
+	add_child(preload("res://Worlds/GlobalModulate.tscn").instantiate())
+
 	glbl.update_layers()
+
 	for layer in glbl.layer:
+
 		dbg.print_header(str(self),str(layer))
+
 		for inst in self.get_node(layer).get_children():
 			if inst is InstancePlaceholder:
-				printt(
-					"|", str(Time.get_datetime_string_from_system(false,true)," Instancing: " + str(inst))
-					)
+				printt("|", str(Time.get_datetime_string_from_system(false,true)," Instancing: " + str(inst)))
 				inst.create_instance()
+
 		print()
 
+	get_node(pseudo_3d_generator).GenerateLayers()
 
-
-#
-#@export_category("Dialogue")
-#@export_multiline var dialogue_lines : Array[String]
-
-#func _ready():
-#	for dbg_itm in get_tree().get_nodes_in_group("DBG"):
-#		dbg_itm.queue_free()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#@export var character : Array[chr]
-
-#@export_category	("Interractable")
-#@export var			one_time = false
-#@export_file("*.txt") var dialogue_file
-#
-#@export_group("Return") 
-#@export var Give_Item : bool = false
-#@export var Item : glbl.item_id
-#@export var Give_Signal : bool = false
-#@export var Signal_ : String
-#
-#@export_category("Gate/Door")
-#@export_node_path("Node2D") var destination
-#@export var Locked = false
-#@export var key = glbl.item_id
-
-
-
-
-
-
+	var global_occluder_polygon			= OccluderPolygon2D.new()
+	global_occluder_polygon.polygon		= get_node(global_wall).get_child(0).polygon
+	get_node(global_occluder).occluder	= global_occluder_polygon
 
