2cc702daadaa7d47e759d237ecc1980d61c5f055
nnda <96333146+nndda@users.noreply.github.com>
2023-11-07 01:09:53 +0800

    Increase maximum `health`
    - Add `hit`, and `destroyed` signals
    - Make particles optional

--- Worlds/Objects/Destructibles/DestructiblesBody.gd
+++ Worlds/Objects/Destructibles/DestructiblesBody.gd
@@ -1,19 +1,27 @@
 extends PhysicsBody2D
 
-@export_range( 1.0, 80.0 ) var health : float = 40.0
+@export_range( 1.0, 300.0 ) var health : float = 40.0
 @export_file("*.tscn") var particles_scene : String
 @export_node_path("Node2D") var root_node : NodePath
 
 var particles : Array
 
-func _ready() -> void: for p in 3: particles.append(
-    load( particles_scene ).instantiate() )
+signal hit
+signal destroyed
+
+func _ready() -> void:
+    if !particles_scene.is_empty():
+        for p in 3: particles.append(
+            load( particles_scene ).instantiate() )
 
 func damage( power : float ) -> void:
     print( dbg.value_is( "damage", power ) )
     emit_particles()
     health -= power
-    if health <= 0: get_node( root_node ).queue_free()
+    emit_signal( "hit" )
+    if health <= 0:
+        emit_signal( "destroyed" )
+        get_node( root_node ).queue_free()
 
 func emit_particles() -> void:
     if particles.size() > 0:
