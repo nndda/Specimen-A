d92b1543919a74d8be88038442e0d669d2ee3516
nnda <96333146+nndda@users.noreply.github.com>
2023-11-16 22:01:26 +0800

    Improve shield addon
    - Separate sprite
    - Adjustable health
    - Add light as health indicator

--- Entities/Weapons/Shield.gd
+++ Entities/Weapons/Shield.gd
@@ -2,6 +2,17 @@ extends Node2D
 
 @export_node_path("Node") var root_
 @onready var root : Node = get_node( root_ )
+@onready var lights : Sprite2D = $Sprite2D/Sprite2D
+@onready var physics_body : PhysicsBody2D = $StaticBody2D
+@onready var health : float
 
 func _ready():  if root.immune != null: root.immune = true
 func _exit_tree(): if root.immune != null: root.immune = false
+
+func _on_physic_body_ready():
+    health = $StaticBody2D.health
+
+func _on_physic_body_hit():
+    lights.modulate.a = remap(
+        physics_body.health, health, 0.0, 1.0, 0.08
+    )
