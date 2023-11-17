425250f239926dd59f478376c58e37133f4620a4
nnda <96333146+nndda@users.noreply.github.com>
2023-11-17 23:26:18 +0800

    Add hit spark particles

--- Entities/Weapons/Shield.gd
+++ Entities/Weapons/Shield.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export_node_path("Node") var root_
 @onready var root : Node = get_node( root_ )
 @onready var lights : Sprite2D = $Sprite2D/Sprite2D
+@onready var hit_spark : GPUParticles2D = $BulletSpark
 @onready var physics_body : PhysicsBody2D = $StaticBody2D
 @onready var health : float
 
@@ -13,6 +14,7 @@ func _on_physic_body_ready():
     health = $StaticBody2D.health
 
 func _on_physic_body_hit():
+    hit_spark.emitting = true
     lights.modulate.a = remap(
         physics_body.health, health, 0.0, 1.0, 0.08
     )
