484cde18805737cd69111222b860129766174515
nnda <96333146+nndda@users.noreply.github.com>
2023-11-30 23:08:28 +0800

    Cache `top_scale` and improve readability

--- Scripts/PolygonPerspective.gd
+++ Scripts/PolygonPerspective.gd
@@ -1,6 +1,6 @@
 extends Polygon2D
 
-# create a sprite, convert to polygon2d, n then attach this script
+# Create a sprite, convert to polygon2d, n then attach this script
 
 enum SkewDirection { RIGHT, DOWN, LEFT, UP }
 @export var direction : SkewDirection = SkewDirection.UP
@@ -12,6 +12,7 @@ var texture_rel : PackedVector2Array
 var texture_size : Vector2
 var texture_dir : Vector2
 var polygon_idx : Vector2i
+var top_scale : float
 
 func get_polygon_idx( vec_dir : Vector2i ) -> Vector2i:
     var rad : float = atan2( vec_dir.y, vec_dir.x )
@@ -23,6 +24,7 @@ func get_polygon_idx( vec_dir : Vector2i ) -> Vector2i:
 
 func _ready() -> void:
 
+    top_scale = Global.top_scale
     texture_size = texture.get_size()
 
     match direction:
@@ -32,7 +34,7 @@ func _ready() -> void:
                 ( texture_size * 0.5 ) * Vector2( 1, 1 ),
                 ( texture_size * 0.5 ) * Vector2( -1, 1 ),
             ]
-            
+
         SkewDirection.RIGHT:   # 1,    1, 0
             texture_dir = Vector2.RIGHT * texture_size
             texture_rel = [
@@ -56,16 +58,16 @@ func _ready() -> void:
 
     polygon_idx = get_polygon_idx( texture_dir.sign() )
 
-func _process( _delta ) -> void: if visible:
+func _process( _delta ) -> void:
+    if visible:
+        canvas_pos = ( ( \
+            get_global_transform_with_canvas().origin * 2 ) /\
+            viewport_rect_size ) - Vector2.ONE
 
-    canvas_pos = ( ( \
-        get_global_transform_with_canvas().origin * 2 ) /\
-        viewport_rect_size ) - Vector2.ONE
+        canvas_rel =\
+            texture_dir +\
+            ( canvas_pos * texture_size ) +\
+            ( canvas_pos * top_scale )
 
-    canvas_rel =\
-        texture_dir +\
-        ( canvas_pos * texture_size ) +\
-        ( canvas_pos * Global.top_scale )
-
-    polygon[ polygon_idx.x ] = canvas_rel + ( position + texture_rel[ 0 ] )
-    polygon[ polygon_idx.y ] = canvas_rel + ( position + texture_rel[ 1 ] )
+        polygon[ polygon_idx.x ] = canvas_rel + ( position + texture_rel[ 0 ] )
+        polygon[ polygon_idx.y ] = canvas_rel + ( position + texture_rel[ 1 ] )
