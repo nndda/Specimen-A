959629d56f9b27078b8c8281a2ac85dc6670d0db
nnda <96333146+nndda@users.noreply.github.com>
2023-12-03 02:48:11 +0800

    Refactor particles

--- Shaders/Particles/ParticlesHandler.gd
+++ Shaders/Particles/ParticlesHandler.gd
@@ -1,41 +1,35 @@
-extends Node2D
+extends GPUParticles2D
 
 @export var stay : bool = false
 
-@export var custom_init_pos : bool = false
+@export\
+var custom_init_pos : bool = false
 var init_pos : Vector2
 
 @onready var copytimer : Timer = Timer.new()
-#func _enter_tree():
-#	var one_shot_v : bool
-#	var lifetime_v : float
-#	var emitting : bool
 
-#func _enter_tree():
+func emit() -> void:
+    visible = true
+    copytimer.start(lifetime - 0.05)
+    emitting = true
 
 func _ready():
 
-#	if get_parent() != Global.layer_dict["Objects/Particles"]:
-#		$VisibleOnScreenEnabler2D.queue_free()
-    self.show()
+    if custom_init_pos:
+        global_position = init_pos
 
-    if custom_init_pos: global_position = init_pos
+    copytimer.\
+    one_shot = stay
+    one_shot = stay
+    add_child(copytimer)
 
-    self.one_shot = stay
-    copytimer.one_shot = stay
-    copytimer.wait_time = self.lifetime * self.speed_scale + 0.05
-    self.add_child( copytimer )
-    self.emitting = true
-
-var ot : bool = true
+    emit()
 
 func _process( _delta ) -> void:
-    if self.emitting:
+    if emitting:
+        if stay:
+            speed_scale = copytimer.time_left / copytimer.wait_time
 
-        if ot:
-            copytimer.start(self.lifetime)
-            ot = false
-
-        if stay: self.speed_scale = copytimer.time_left / copytimer.wait_time
-
-    if self.speed_scale == 0: self.process_mode = Node.PROCESS_MODE_DISABLED
+        if speed_scale == 0:
+            copytimer.queue_free()
+            process_mode = Node.PROCESS_MODE_DISABLED
