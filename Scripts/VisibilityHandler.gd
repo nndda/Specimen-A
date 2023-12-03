193336de0b1f701ed4aa4c3d6a5448957953acfb
nnda <96333146+nndda@users.noreply.github.com>
2023-12-04 04:59:08 +0800

    Connect signals on runtime
    - Fix shadowed variable `set_process_mode`
    - Clean up

--- Scripts/VisibilityHandler.gd
+++ Scripts/VisibilityHandler.gd
@@ -1,13 +1,16 @@
 extends Node
 
-@export var set_process_mode : Node.ProcessMode = Node.PROCESS_MODE_INHERIT
-@export_node_path( "CanvasItem" ) var root_path : NodePath
+@export var set_process_mode_to : Node.ProcessMode = Node.PROCESS_MODE_INHERIT
+@export_node_path("CanvasItem") var root_path : NodePath
 @onready var root : CanvasItem = get_parent() if root_path.is_empty() else get_node( root_path )
 
-func _on_ready():
+func _ready():
     $VisibleOnScreenEnabler2D.global_position = root.global_position
     $VisibleOnScreenEnabler2D.global_rotation = root.global_rotation
+    $VisibleOnScreenEnabler2D.screen_entered.connect(_on_screen_entered)
+    $VisibleOnScreenEnabler2D.screen_exited.connect(_on_screen_exited)
     root.visible = false
+    root.process_mode = set_process_mode_to
 
 func _on_screen_entered():
     root.visible = true
@@ -15,4 +18,4 @@ func _on_screen_entered():
 
 func _on_screen_exited():
     root.visible = false
-    root.process_mode = set_process_mode
+    root.process_mode = set_process_mode_to
