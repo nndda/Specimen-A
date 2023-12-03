5ea7ebdd698c4e44455c3c0039121b0727dec337
nnda <96333146+nndda@users.noreply.github.com>
2023-12-04 05:04:25 +0800

    Fix player damage system
    Player receiving far too much damage from shooting gun than intended

--- Entities/Weapons/Gun-Rifle.gd
+++ Entities/Weapons/Gun-Rifle.gd
@@ -1,42 +1,59 @@
 extends Node
 
-@export_node_path( "AnimationPlayer" ) var animation_player_ : NodePath
-@onready var animation_player : AnimationPlayer = get_node( animation_player_ )
+@export_node_path("AnimationPlayer") var animation_player_ : NodePath
+@onready var animation_player : AnimationPlayer = get_node(animation_player_)
 
-@onready var wielder = $"../.."
-@onready var weapon = $".."
+@onready var wielder := $"../.."
+@onready var weapon := $".."
 
-@onready var line_of_sight = $"../LineOfSight"
-@onready var line_of_fire = $"../LineOfSight/LineOfFire"
-@onready var bullet_path = $"../BulletPath"
-@onready var bullet_spark = $"../BulletSpark"
+@onready var line_of_sight := $"../LineOfSight"
+@onready var line_of_fire := $"../LineOfSight/LineOfFire"
+@onready var line_of_fire_const := $"../LineOfSight/LineOfFire/Const"
+@onready var bullet_path := $"../BulletPath"
+@onready var bullet_spark := $"../BulletSpark"
 
-@onready var colliding_point = $"../CollidingPoint"
+@onready var colliding_point := $"../CollidingPoint"
+
+var hit := false
+
+func _ready() -> void:
+    animation_player.animation_started.connect(firing_started)
+    animation_player.animation_finished.connect(firing_ended)
 
 func fire() -> void:
     randomize()
-    Camera.shake_start( 5.0, 0.09, 32.0 )
-    line_of_fire.rotation_degrees   = randf_range( -2.25, 2.25 )
-    bullet_path.rotation_degrees    = line_of_fire.rotation_degrees
+    Camera.shake_start(5.0, 0.09, 32.0)
+    line_of_fire.rotation_degrees = randf_range(-2.25, 2.25)
+    bullet_path.rotation_degrees = line_of_fire.rotation_degrees
+
+func firing_started(anim : StringName) -> void:
+    if anim == &"Firing":
+        bullet_path.visible = true
+        line_of_fire_const.enabled = true
+func firing_ended(anim : StringName) -> void:
+    if anim == &"Firing":
+        bullet_path.visible = false
+        line_of_fire_const.enabled = false
 
 func _physics_process(_delta):
     if wielder.triggered:
+            hit = line_of_fire.is_colliding()
+            bullet_path.points[0] = Vector2.ZERO
+            bullet_spark.visible = hit
+            #bullet_path.visible = animation_player.is_playing()# and bullet_spark.visible
 
-            bullet_path.points[0]   = Vector2.ZERO
-            bullet_spark.visible    = line_of_fire.is_colliding()
-            bullet_path.visible     = animation_player.is_playing()# and bullet_spark.visible
+            #line_of_fire_const.enabled = animation_player.is_playing()
 
-            line_of_fire.enabled    = animation_player.is_playing()
-
-            if line_of_fire.is_colliding():
-                colliding_point.global_position = line_of_fire.get_collision_point()
+            if hit:
+                colliding_point.global_position = line_of_fire_const.get_collision_point()
                 bullet_path.points[1] = colliding_point.position
 
                 bullet_spark.global_position = colliding_point.global_position
                 if line_of_fire.get_collider() != null:
-                    if line_of_fire.get_collider().has_method( "damage_player" ):
-                        line_of_fire.get_collider().damage_player( randf_range( 8, 12 ) )
+                    if line_of_fire.get_collider().has_method(&"damage_player"):
+                        line_of_fire.get_collider().damage_player(randf_range(
+                                weapon.damage_min, weapon.damage_max))
 
             else:
-                bullet_path.points[1] = Vector2(0,110)
+                bullet_path.points[1] = Vector2(0, 110)
                 bullet_path.points[1] = $"../CollidingPointDefault".position
--- Entities/Weapons/Gun.gd
+++ Entities/Weapons/Gun.gd
@@ -1,22 +1,27 @@
 extends Node2D
 
-var damage : float = 4.25
-var firing : bool = false
+## Base script for ALL gun/projectile addon
+# NOTE : Require `FireFunction` Node with variable `animation_player`, containing animation named "Firing"
+
+@export var damage_min : float = 2.25
+@export var damage_max : float = 4.5
+var firing := false
+
+@onready var fire_function := $FireFunction
+@onready var fire_function_anim : AnimationPlayer = $FireFunction.animation_player
 
 @export_node_path( "Area2D", "RayCast2D" ) var line_of_sight_; var line_of_sight : Node2D
 @export_node_path( "Area2D", "RayCast2D" ) var friendly_sight_; var friendly_sight : Node2D
 
 var parent : Node
 var trigger_area : NodePath
-@onready var on_line : bool = false
+var on_line := false
 
 var player_bodies : Array[Node2D] = []
 
 func _on_line( object ) -> void:
-#    on_line = [ "Head", "DamageCollision" ].has( object.get_name() )
     on_line = player_bodies.has( object )
 func _off_line( object ) -> void:
-#    on_line = ![ "Head", "DamageCollision" ].has( object.get_name() )
     on_line = !player_bodies.has( object )
 
 func _enter_tree():
@@ -29,15 +34,8 @@ func _ready() -> void:
 
     trigger_area = NodePath( "../TriggerArea" if parent.custom_trigger == null else parent.custom_trigger_ )
 
-#    if parent.custom_trigger == null:
-#        trigger_area = NodePath( "../TriggerArea" )
-#    else:
-#        trigger_area = NodePath( parent.custom_trigger_ )
-
     if  line_of_sight != null and\
         line_of_sight is CollisionObject2D:
-
-#        printt( "", self.get_name(), "line_of_sight : " + str( line_of_sight.get_class() ) )
 
         if line_of_sight is RayCast2D:
             line_of_sight.add_exception( get_node( trigger_area ) )
@@ -50,16 +48,13 @@ func _ready() -> void:
             for signal_off in [ "body_exited", "area_exited" ]:
                 line_of_sight.connect( signal_off, Callable( self, "_off_line" ) )
 
-#    else:
-#        push_error( dbg.value_is( "weapon_node", "null" ) )
+    fire_function_anim.connect(
+        &"animation_finished", Callable( parent, &"Weapon_AnimationFinished" ) )
 
-    $FireFunction.animation_player.connect(
-        "animation_finished", Callable( parent, "Weapon_AnimationFinished" ) )
-
-func _process( _delta ) -> void:
+func _process(_delta) -> void:
     parent.firing = firing
 
-func _physics_process( _delta ) -> void:
+func _physics_process(_delta) -> void:
     if parent.triggered:
         if line_of_sight is RayCast2D:
             on_line = line_of_sight.is_colliding() and !friendly_sight.is_colliding()
