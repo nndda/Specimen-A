f32f5218a6c79ea944ea6078640c36b757c6b5ce
nnda <96333146+nndda@users.noreply.github.com>
2023-12-04 05:01:58 +0800

    Update base Light scene

--- Shaders/Lights/Light.gd
+++ Shaders/Lights/Light.gd
@@ -4,8 +4,8 @@ extends Node2D
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 func _ready():
-    connect( "visibility_changed", func(): light.enabled = visible )
-    Global.connect( "camera_shaken_by_player", func():\
+    visibility_changed.connect( func(): light.enabled = visible )
+    Global.camera_shaken_by_player.connect( func():\
     if visible:
         if global_position.distance_to( Global.head_pos ) <= 360.0:
             animation_player.play( "Flick_" + [ "0", "1" ].pick_random() ) )
