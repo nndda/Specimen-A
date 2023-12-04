1d5f1cb26f5399d174d4f89a8da44b40b19a3035
nnda <96333146+nndda@users.noreply.github.com>
2023-12-05 05:22:38 +0800

    Update pause menu

--- UI/PauseMenu.gd
+++ UI/PauseMenu.gd
@@ -1,12 +1,16 @@
 extends CanvasLayer
 
-var paused : bool = false
-@onready var level_root : Node2D = $".."
+var paused := false
+@onready var level_root := $".."
 
-func _enter_tree():
+@onready var level_name := $Control/Area/LevelName
+@onready var level_sub := $Control/Area/LevelName/Sub
+
+func _enter_tree() -> void:
+    visible =false
     Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 
-func _input(event):
+func _input(event : InputEvent) -> void:
     if event.is_action_pressed(&"Pause"):
         paused = !paused
         visible = paused
@@ -18,5 +22,6 @@ func _input(event):
             Input.MOUSE_MODE_VISIBLE if paused else\
             Input.MOUSE_MODE_CONFINED
 
-func _ready():
-    $Control/LevelName.text = level_root.level_name
+func _ready() -> void:
+    level_name.text = level_root.level_name
+    level_sub.text = ""
