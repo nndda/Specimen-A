72c75fb46d681c02c06755100072a7b5d0095a54
nnda <96333146+nndda@users.noreply.github.com>
2023-12-06 12:29:15 +0800

    Fix top layer and connect `Camera`
    - Add `Area` and top layer `Lights` section for the base scene
    - Clean up

--- Levels/LevelHandler.gd
+++ Levels/LevelHandler.gd
@@ -3,31 +3,27 @@ extends Node2D
 @export var level_name : StringName
 @export_node_path("Node2D") var pseudo_3d_generator : NodePath
 
+signal level_loaded
+
 func _enter_tree() -> void:
     Global.current_scene = self
     Global.update_layers()
+    level_loaded.connect(Camera.start_fade_out)
 
 func _ready() -> void:
-    var t1 : int = Time.get_ticks_msec()
-    for c in get_children(): if c is CanvasItem: c.show()
-    add_child( preload( "res://Worlds/GlobalModulate.tscn" ).instantiate() )
+    add_child(preload("res://Worlds/GlobalModulate.tscn").instantiate())
+
+    $TopLayer.visible = true
+    $"TopLayer+1".visible = true
+    for i in get_children():
+        if i is CanvasItem:
+            i.visible = true
 
     for layer in Global.layer:
-        dbg.print_header( self, layer )
-        for inst in self.get_node(layer).get_children():
-            if inst is InstancePlaceholder:
-                printt(" Instancing: " + str( inst ) )
-                inst.create_instance()
-                inst.queue_free()
-        print()
-
-    for layer in Global.layer:
-        for inst in self.get_node( layer ).get_children():
-            if inst is CharacterBody2D: Global.current_objects.append( inst )
-
-    Camera.init_visual_loading()
+        for inst in get_node(layer).get_children():
+            if inst is CharacterBody2D:
+                Global.current_objects.append(inst)
 
     get_node(pseudo_3d_generator).call_deferred(&"generate_layers")
-
-    printt( ">", float( Time.get_ticks_msec() - t1 ) / 1000 )
-    printt( ">", Time.get_datetime_string_from_system( false, true ), self )
+    Camera.init_visual_loading()
+    level_loaded.emit()
