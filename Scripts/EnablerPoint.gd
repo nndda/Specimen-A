5c50bf2a6f24d26c1bb3249e4672eae9a8f8ffba
nnda <96333146+nndda@users.noreply.github.com>
2023-12-06 12:21:08 +0800

    Create `EnablerPoint`

---
+++ Scripts/EnablerPoint.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export\
+var entities_path : Array[NodePath] = []
+var entities : Array[Node] = []
+
+var player_entered := false
+
+func _ready():
+    for entity in entities_path:
+        entities.append(get_node(entity))
+
+func _on_body_entered(body : Node2D) -> void:
+    if body.get_name() == &"Head":
+        if !player_entered:
+            player_entered = true
+
+            for entity in entities:
+                entity.process_mode = Node.PROCESS_MODE_INHERIT
+
+            queue_free()
