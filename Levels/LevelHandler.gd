f69b700f47d0cc8acc160ad41bb641f83930e6f3
nnda <96333146+nndda@users.noreply.github.com>
2023-12-08 19:52:17 +0800

    Refactor base level structure

--- Levels/LevelHandler.gd
+++ Levels/LevelHandler.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var level_name : StringName
 @export_node_path("Node2D") var pseudo_3d_generator : NodePath
 
+@onready var top_light_layer := $TopLayer/Lights
+
 signal level_loaded
 
 func _enter_tree() -> void:
@@ -11,7 +13,12 @@ func _enter_tree() -> void:
     level_loaded.connect(Camera.start_fade_out)
 
 func _ready() -> void:
-    add_child(preload("res://Worlds/GlobalModulate.tscn").instantiate())
+    for top_light in get_tree().get_nodes_in_group(&"top_light"):
+        top_light.call_deferred(&"reparent", top_light_layer)
+
+    get_node(pseudo_3d_generator).call_deferred(&"generate_layers")
+    call_deferred(&"add_child", preload("res://Worlds/GlobalEnvironment.tscn").instantiate())
+    call_deferred(&"add_child", preload("res://Worlds/GlobalModulate.tscn").instantiate())
 
     $TopLayer.visible = true
     $"TopLayer+1".visible = true
@@ -24,6 +31,6 @@ func _ready() -> void:
             if inst is CharacterBody2D:
                 Global.current_objects.append(inst)
 
-    get_node(pseudo_3d_generator).call_deferred(&"generate_layers")
     Camera.init_visual_loading()
     level_loaded.emit()
+
