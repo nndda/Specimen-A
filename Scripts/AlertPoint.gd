d8b7517fe06aff38e0b7717de3255077770e9960
nnda <96333146+nndda@users.noreply.github.com>
2023-12-10 23:21:48 +0800

    Create `AlertPoint`

---
+++ Scripts/AlertPoint.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+@onready var root : Node = get_parent()
+
+@export\
+var items_path : Array[NodePath] = []
+var items : Array[Node] = []
+
+var player_entered := false
+
+func _ready():
+    body_entered.connect(_on_body_entered)
+    root.ready.connect(func():
+        for item : NodePath in items_path:
+            var node : Node = get_node(item)
+            if !node.has_signal(&"triggered"):
+                push_error("Item %s does not have `triggered` signal" % str(node))
+            else:
+                items.append(node)
+    )
+
+func _on_body_entered(body : Node2D) -> void:
+    if body.get_name() == &"Head":
+        if !player_entered:
+            player_entered = true
+        alert()
+
+func alert() -> void:
+    for item : Node in items:
+        item.triggered.emit()
+    queue_free()
