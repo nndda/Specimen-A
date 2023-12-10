09a151d197d5de54347311bce639c44e68bba41c
nnda <96333146+nndda@users.noreply.github.com>
2023-12-10 23:22:21 +0800

    Improve readability

--- Scripts/EnablerPoint.gd
+++ Scripts/EnablerPoint.gd
@@ -1,21 +1,27 @@
 extends Area2D
 
+@onready var root : Node = get_parent()
+
 @export\
-var entities_path : Array[NodePath] = []
-var entities : Array[Node] = []
+var items_path : Array[NodePath] = []
+var items : Array[Node] = []
 
 var player_entered := false
 
 func _ready():
-    for entity : NodePath in entities_path:
-        entities.append(get_node(entity))
+    body_entered.connect(_on_body_entered)
+    root.ready.connect(func():
+        for item : NodePath in items_path:
+            items.append(get_node(item))
+    )
 
 func _on_body_entered(body : Node2D) -> void:
     if body.get_name() == &"Head":
         if !player_entered:
             player_entered = true
+        enable()
 
-            for entity : Node in entities:
-                entity.process_mode = Node.PROCESS_MODE_INHERIT
-
-            queue_free()
+func enable() -> void:
+    for item : Node in items:
+        item.process_mode = Node.PROCESS_MODE_INHERIT
+    queue_free()
