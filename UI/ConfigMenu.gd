3afe5c348a4aca0d5134ae0375911944b82b93ae
nnda <96333146+nndda@users.noreply.github.com>
2023-12-10 10:01:58 +0800

    Flesh out #3

--- UI/ConfigMenu.gd
+++ UI/ConfigMenu.gd
@@ -5,16 +5,19 @@ func sync_config() -> void:
     fullscreen_toggle.button_pressed = Global.user_data["config"]["fullscreen"]
     if !Global.user_data["config"]["fullscreen"]:
         resolution_button.select(Global.user_data["config"]["resolution_idx"])
+
     audio_master.value = Global.user_data["config"]["master"]
-    _on_master_changed(audio_master.value)
+    audio_master.value_changed.emit(audio_master.value)
+
     audio_sfx.value = Global.user_data["config"]["sfx"]
-    _on_sfx_changed(audio_sfx.value)
+    audio_sfx.value_changed.emit(audio_sfx.value)
+
     audio_bgm.value = Global.user_data["config"]["bgm"]
-    _on_bgm_changed(audio_bgm.value)
+    audio_bgm.value_changed.emit(audio_bgm.value)
 
 func _on_visibility_changed() -> void:
     if visible == true:
-        $Menu/Buttons/Back.grab_focus()
+        back_button.grab_focus()
 
 func _enter_tree() -> void:
     visible = false
@@ -23,21 +26,56 @@ func _input(event : InputEvent) -> void:
     if event.is_action_pressed(&"Pause"):
         if visible: visible = false
 
+func _ready() -> void:
+    reset_confirm.hide()
+    reset_progress_confirm.hide()
+    rebind_dialogue.hide()
+
+    for action in [&"Attack",&"Move"]:
+        rebind_buttons[action].text = Global["config"][action].as_text_keycode()
+
+
+## Main buttons
+@onready var back_button : Button = $Menu/Buttons/Back
+func back_pressed() -> void:
+    if visible:
+        visible = false
+        Global.update_user_data()
+
+@onready var reset_confirm : ConfirmationDialog = $Menu/Buttons/Reset/ConfirmationDialog
+func reset_settings_pressed() -> void:
+    reset_confirm.popup_centered()
+func reset_settings_confirmed() -> void:
+    Global.user_data["config"] = Global["config"].user_data_default
+    Global.update_user_data()
+    sync_config()
+
+@onready var reset_progress_confirm : ConfirmationDialog = $Menu/Buttons/Right/ResetProgress/ConfirmationDialog
+func reset_progress_pressed() -> void:
+    reset_progress_confirm.popup_centered()
+func reset_progress_confirmed() -> void:
+    Global.user_data["progress"] = Global["progress"].user_data_default
+    Global.user_data["achievements"] = Global["achievements"].user_data_default
+    Global.update_user_data()
+    sync_config()
+
+
 #### DISPLAY
 
 ### FULLSCREEN
-@onready var fullscreen_toggle := $Menu/Tab/Display/Items/Fullscreen/CheckButton
-func _on_fullscreen_toggled(toggled_on : bool) -> void:
+@onready var fullscreen_toggle : CheckButton = $Menu/Tab/Display/Items/Fullscreen/CheckButton
+func display_fullscreen_toggled(toggled_on : bool) -> void:
     DisplayServer.window_set_mode(
-        DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN if toggled_on else\
+        DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN if toggled_on else
         DisplayServer.WINDOW_MODE_WINDOWED
     )
     resolution_button.disabled = toggled_on
     Global.user_data["config"]["fullscreen"] = toggled_on
 
 ### SCREEN/DISPLAY RESOLUTION
-@onready var resolution_button := $Menu/Tab/Display/Items/Resolution/OptionButton
-func _on_resolution_selected(index : int) -> void:
+var environment := preload("res://Worlds/GlobalEnviroment.tres")
+@onready var resolution_button : OptionButton = $Menu/Tab/Display/Items/Resolution/OptionButton
+func display_resolution_selected(index : int) -> void:
     var res_string : PackedStringArray =\
         resolution_button.get_item_text(index).split(" x ")
     DisplayServer.window_set_size(Vector2i(
@@ -53,8 +91,6 @@ func adjust_contrast(value : float) -> void:
     environment.adjustment_contrast = value
     Global.user_data["config"]["contrast"] = value
 
-var environment := load("res://Worlds/GlobalEnviroment.tres")
-
 
 #### AUDIO
 func display_volume(value : float, label : Label) -> void:
@@ -63,46 +99,86 @@ func display_volume(value : float, label : Label) -> void:
         int(remap(value, -80.0, 2.0, 0.0, 100.0))
     ) + "%"
 
-@onready var audio_master := $Menu/Tab/Sound/Items/Master/HSlider
-@onready var audio_master_label := $Menu/Tab/Sound/Items/Master/Labels/Value
-func _on_master_changed(value : float) -> void:
+@onready var audio_master : HSlider = $Menu/Tab/Sound/Items/Master/HSlider
+@onready var audio_master_label : Label = $Menu/Tab/Sound/Items/Master/Labels/Value
+func audio_master_changed(value : float) -> void:
     AudioServer.set_bus_volume_db(0, value)
     display_volume(value, audio_master_label)
     Global.user_data["config"]["master"] = value
 
-@onready var audio_sfx := $Menu/Tab/Sound/Items/SFX/HSlider
-@onready var audio_sfx_label := $Menu/Tab/Sound/Items/SFX/Labels/Value
-func _on_sfx_changed(value : float) -> void:
+@onready var audio_sfx : HSlider = $Menu/Tab/Sound/Items/SFX/HSlider
+@onready var audio_sfx_label : Label = $Menu/Tab/Sound/Items/SFX/Labels/Value
+func audio_sfx_changed(value : float) -> void:
     AudioServer.set_bus_volume_db(1, value)
     display_volume(value, audio_sfx_label)
     Global.user_data["config"]["sfx"] = value
 
-@onready var audio_bgm := $Menu/Tab/Sound/Items/BGM/HSlider
-@onready var audio_bgm_label := $Menu/Tab/Sound/Items/BGM/Labels/Value
-func _on_bgm_changed(value : float) -> void:
+@onready var audio_bgm : HSlider = $Menu/Tab/Sound/Items/BGM/HSlider
+@onready var audio_bgm_label : Label = $Menu/Tab/Sound/Items/BGM/Labels/Value
+func audio_bgm_changed(value : float) -> void:
     AudioServer.set_bus_volume_db(2, value)
     display_volume(value, audio_bgm_label)
     Global.user_data["config"]["bgm"] = value
 
 
-func _back_pressed() -> void:
-    if visible:
-        visible = false
-        Global.update_user_data()
+## CONTROLS
 
-@onready var reset_confirm := $Menu/Buttons/Reset/ConfirmationDialog
-func _reset_pressed() -> void:
-    reset_confirm.popup_centered()
-func _reset_confirmed() -> void:
-    Global.user_data["config"] = Global["config"].user_data_default
-    Global.update_user_data()
-    sync_config()
+@onready var rebind_buttons := {
+    &"Move" : $Menu/Tab/Controls/Items/Move/Actions/Rebind,
+    &"Attack" : $Menu/Tab/Controls/Items/Attack/Actions/Rebind,
+}
+@onready var rebind_dialogue : Window = $Menu/Tab/Controls/RebindDialogue
+@onready var rebind_key_label : Label = $Menu/Tab/Controls/RebindDialogue/PanelContainer/Control/VBoxContainer/Key
+var rebind_target := &""
+var rebind_old : InputEvent
+var rebind_new : InputEvent
+const forbidden_keys : Array[Key] = [
+    KEY_ESCAPE,
+    KEY_F1,
+    KEY_F2,
+    KEY_F3,
+    KEY_F4,
+    KEY_F5,
+    KEY_F6,
+    KEY_F7,
+    KEY_F8,
+    KEY_F9,
+    KEY_F10,
+    KEY_F11,
+    KEY_F12,
+    KEY_PRINT,
+    KEY_SCROLLLOCK,
+    KEY_NUMLOCK,
+    KEY_PAUSE,
+    KEY_CTRL,
+    KEY_ALT,
+    KEY_META,
+    KEY_MENU,
+]
 
-@onready var reset_progress_confirm := $Menu/Buttons/Right/ResetProgress/ConfirmationDialog
-func _reset_progress_pressed() -> void:
-    reset_progress_confirm.popup_centered()
-func _reset_progress_confirmed() -> void:
-    Global.user_data["progress"] = Global["progress"].user_data_default
-    Global.user_data["achievements"] = Global["achievements"].user_data_default
-    Global.update_user_data()
-    sync_config()
+func rebind_input(event : InputEvent) -> void:
+    if event is InputEventKey:
+        if !forbidden_keys.has(event.keycode):
+            rebind_key_label.text = "[ %s ]" % event.as_text_keycode()
+            rebind_old = InputMap.action_get_events(rebind_target)[1]
+            rebind_new = event
+
+func rebind_close_requested() -> void:
+    rebind_dialogue.hide()
+
+func rebind_confirmed() -> void:
+    InputMap.action_erase_event(rebind_target, rebind_old)
+    InputMap.action_add_event(rebind_target, rebind_new)
+
+    rebind_buttons[rebind_target].text = rebind_new.as_text_keycode()
+    Global.user_data["config"][rebind_target] = rebind_new
+
+    rebind_target = &""
+    rebind_old = null
+    rebind_new = null
+    rebind_dialogue.close_requested.emit()
+
+func rebind_key_pressed(action : StringName) -> void:
+    rebind_key_label.text = "[ Press any key ]"
+    rebind_target = action
+    rebind_dialogue.popup_centered()
