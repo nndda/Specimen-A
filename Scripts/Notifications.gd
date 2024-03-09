1b096afc52be0cbd893b1da75d8d8c512e1741f6
nnda <96333146+nndda@users.noreply.github.com>
2024-03-10 05:13:59 +0800

    Implement level labels display
    - Remove `Item` class. Revert back to using `Dictionary`

--- Scripts/Notifications.gd
+++ Scripts/Notifications.gd
@@ -7,29 +7,45 @@ var notification_desc : RichTextLabel
 @export_node_path("TextureRect") var notification_img_ : NodePath
 var notification_img : TextureRect
 
-var queue : Array = []
+@onready var level_label : Label = $Control/SceneLabels/LevelLabels
+@onready var level_label_sub : Label = $Control/SceneLabels/LevelLabels/Sub
 
-signal notification_fired(item : Item)
+@onready var level_label_anim : AnimationPlayer = $Control/SceneLabels/LevelLabels/AnimationPlayer
+@onready var level_label_anim_sub : AnimationPlayer = $Control/SceneLabels/LevelLabels/AnimationPlayer/Sub
 
-class Item extends Object:
-    var title := ""
-    var desc := ""
-    var img_res := ""
+var queue : Array[Dictionary] = []
 
-    func _init(
-        title_ : String,
-        desc_ : String,
-        img_res_ : String
-        ):
-        title = title_
-        desc = desc_
-        img_res = img_res_
+signal notification_fired(item : Dictionary)
 
 func _ready() -> void:
     notification_title = get_node(notification_title_)
     notification_desc =  get_node(notification_desc_)
 
-func fire_notif(item : Item) -> void:
-    notification_title.text = item.title
-    notification_desc.text = item.desc
-    notification_img.texture = load(item.img_res)
+    level_label_anim.animation_finished.connect(func(_n) -> void:
+        level_label_displayed.emit()
+    )
+    level_label_anim_sub.animation_finished.connect(func(_n) -> void:
+        level_label_sub_displayed.emit()
+    )
+
+func fire_notif(item : Dictionary) -> void:
+    notification_title.text = item["title"]
+    notification_desc.text = item["desc"]
+    notification_img.texture = load(item["img_res"])
+
+signal level_label_displayed
+signal level_label_sub_displayed
+
+func set_level_label(string : String) -> void:
+    level_label.text = string
+    level_label_anim.play(&"Display")
+
+    await get_tree().create_timer(string.length() / 3.25).timeout
+    level_label_anim.play_backwards(&"Display")
+
+func set_level_label_sub(string : String) -> void:
+    level_label_sub.text = string
+    level_label_anim_sub.play(&"Display")
+
+    await get_tree().create_timer(string.length() / 3.25).timeout
+    level_label_anim_sub.play_backwards(&"Display")
