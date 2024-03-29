4094b683bfd6448cdb4db4053bf91f993b9090a7
nnda <96333146+nndda@users.noreply.github.com>
2024-03-29 15:18:34 +0800

    Add explosion trace effects

--- Shaders/Particles/Explosion.gd
+++ Shaders/Particles/Explosion.gd
@@ -1,6 +1,12 @@
 extends GPUParticles2D
 
+@export var leave_trace := true
+@onready var trace_sprite : Sprite2D = $Sprite2D
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+    trace_sprite.visible = false
 
 func explode() -> void:
     animation_player.play(&"explode")
+    trace_sprite.visible = true
