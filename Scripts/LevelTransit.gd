7afde882fbbd84d9daac79ba7934f8ec17b3f7f3
nnda <96333146+nndda@users.noreply.github.com>
2024-03-29 15:34:01 +0800

    Create `LevelTransit`
    Used by the player for transitioning between level

---
+++ Scripts/LevelTransit.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export_file("*.tscn", "*.scn") var level_transition_scene : String
+
+func _on_body_entered(body : Node2D) -> void:
+    if body.get_name() == &"Head":
+        Global.current_scene.set_deferred(&"process_mode", Node.PROCESS_MODE_DISABLED)
+        Camera.start_fade_in()
+        await Camera.faded_in
+        (Global.current_scene.tree as SceneTree).change_scene_to_file(
+            level_transition_scene
+        )
