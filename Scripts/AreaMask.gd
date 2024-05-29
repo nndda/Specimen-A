c90c520f39b6756cdbc1bebb0b131fe6c8c6cce6
nnda <96333146+nndda@users.noreply.github.com>
2024-05-29 20:58:50 +0800

    Disable shader when not visible

--- Scripts/AreaMask.gd
+++ Scripts/AreaMask.gd
@@ -10,9 +10,12 @@ var visibility_tween : Tween
 
 func _ready() -> void:
     visible = true
+    material = material.duplicate()
+    fade_in()
     if area_cover != null:
         area_cover.body_entered.connect(_on_area_cover_body_entered)
         area_cover.body_exited.connect(_on_area_cover_body_exited)
+
 
 func _on_area_cover_body_entered(body : Node2D) -> void:
     if body.name == &"Head":
@@ -24,17 +27,29 @@ func _on_area_cover_body_exited(body : Node2D) -> void:
 
 
 func fade_in(duration : float = 0.5) -> void:
+    (material as ShaderMaterial).set_shader_parameter(&"enabled", true)
+    visible = true
+
     visibility_tween = create_tween().bind_node(self)
     visibility_tween.tween_property(
             self, ^"modulate:a", 1.0, duration
-        ).set_ease(Tween.EASE_OUT).from_current()
+        )\
+        .set_ease(Tween.EASE_OUT)\
+        .set_trans(Tween.TRANS_QUAD)\
+        .from_current()
 
 func fade_out(duration : float = 0.5) -> void:
     visibility_tween = create_tween().bind_node(self)
     visibility_tween.tween_property(
             self, ^"modulate:a", 0.0, duration
-        ).set_ease(Tween.EASE_OUT).from_current()
+        )\
+        .set_ease(Tween.EASE_OUT)\
+        .set_trans(Tween.TRANS_QUAD)\
+        .from_current()
+
+    await visibility_tween.finished
+    visible = false
+    (material as ShaderMaterial).set_shader_parameter(&"enabled", false)
 
     if destroy_on_reveal:
-        await visibility_tween.finished
         queue_free()
