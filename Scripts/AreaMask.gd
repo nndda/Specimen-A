23e7d27e2c5f1558594a394c25651972b94d55bc
nnda <96333146+nndda@users.noreply.github.com>
2024-05-29 17:46:13 +0800

    Create `AreaMask`

---
+++ Scripts/AreaMask.gd
@@ -0,0 +1,40 @@
+extends Polygon2D
+
+@export var area_cover : Area2D
+@export var destroy_on_reveal := false
+
+@export_category("Durations")
+@export var fade_in_duration : float = 0.5
+@export var fade_out_duration : float = 0.5
+var visibility_tween : Tween
+
+func _ready() -> void:
+    visible = true
+    if area_cover != null:
+        area_cover.body_entered.connect(_on_area_cover_body_entered)
+        area_cover.body_exited.connect(_on_area_cover_body_exited)
+
+func _on_area_cover_body_entered(body : Node2D) -> void:
+    if body.name == &"Head":
+        fade_out(fade_out_duration)
+
+func _on_area_cover_body_exited(body : Node2D) -> void:
+    if body.name == &"Head":
+        fade_in(fade_in_duration)
+
+
+func fade_in(duration : float = 0.5) -> void:
+    visibility_tween = create_tween().bind_node(self)
+    visibility_tween.tween_property(
+            self, ^"modulate:a", 1.0, duration
+        ).set_ease(Tween.EASE_OUT).from_current()
+
+func fade_out(duration : float = 0.5) -> void:
+    visibility_tween = create_tween().bind_node(self)
+    visibility_tween.tween_property(
+            self, ^"modulate:a", 0.0, duration
+        ).set_ease(Tween.EASE_OUT).from_current()
+
+    if destroy_on_reveal:
+        await visibility_tween.finished
+        queue_free()
