1322bf79d5dbd4bd860bfd68b8f72bd77376c52f
nnda <96333146+nndda@users.noreply.github.com>
2024-05-29 13:33:57 +0800

    Refactor main menu variables

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -1,27 +1,35 @@
 extends Node2D
 
-@export_category( "Main Buttons" )
-@export_node_path( "BaseButton" ) var start     : NodePath
-@export_node_path( "BaseButton" ) var config    : NodePath
-@export_node_path( "BaseButton" ) var exit      : NodePath
+@export_category("Main Buttons")
+@export var btn_start : Button
+@export var btn_config : Button
+@export var btn_credits : Button
+@export var btn_achievement : Button
+@export var btn_exit : Button
 
-@onready var config_menu := $CanvasLayer/Control/ConfigMenu
+@export_category("Pop Up Windows")
+@export var config_menu : Control
+@export var exit_confirm : AcceptDialog
 
 func _ready() -> void:
     Global.current_scene = self
     Camera.enabled = false
     Camera.start_fade_out()
+
     $Camera2D.enabled = true
     config_menu.sync_config()
 
-    Audio.set_dialogue_window($CanvasLayer/Control/Margin/Menu/Exit/ConfirmationDialog)
+    Audio.set_dialogue_window(exit_confirm)
     Audio.connect_audio()
 
 func _on_exit_pressed() -> void:
-    $Control/VBoxContainer/Exit/ConfirmationDialog.popup_centered()
-func _config_pressed():
+    exit_confirm.popup_centered()
+
+func _config_pressed() -> void:
     config_menu.visible = true
-func _on_exit_confirmation_dialog_confirmed() -> void: get_tree().quit()
+
+func _on_exit_confirmation_dialog_confirmed() -> void:
+    get_tree().quit()
 
 
 
