cf7e21e9803699cd4b81d9f778dfd161792d136c
nnda <96333146+nndda@users.noreply.github.com>
2024-05-29 21:00:34 +0800

    Implement minor UI effects
    - Zoom out when holding attack
    - Fade out opacity on attack cooldown indicator

--- Player/Player.UI.gd
+++ Player/Player.UI.gd
@@ -8,6 +8,9 @@ extends CanvasLayer
 @onready var health_ticker : Timer = $HealthBar/HealthTicker
 @onready var health_visible_timer : Timer = $HealthBar/VisibleTimer
 
+@onready var attack_indicator : TextureProgressBar = $AttackIndicator
+@onready var attack_cooldown : TextureProgressBar = $AttackCooldown
+
 func _ready() -> void:
     health_bar.modulate = Color.TRANSPARENT
     head.damaged.connect(player_damaged)
@@ -15,6 +18,23 @@ func _ready() -> void:
 func _process(_delta : float) -> void:
     health_bar.scale.x = remap(head.health, head.health_max, 0.0, 1.0, 0.0)
     health_low_overlay.modulate.a = absf(health_bar.scale.x - 1.0)
+
+    if !Camera.is_copying:
+        Camera.zoom.x = remap(
+            attack_indicator.value,
+            attack_indicator.min_value,
+            attack_indicator.max_value,
+            1.0, 0.8
+        )
+        Camera.zoom.y = Camera.zoom.x
+
+    if (head.attack_cooldown_timer as Timer).time_left > 0:
+        attack_cooldown.modulate.a = remap(
+            attack_cooldown.value,
+            attack_cooldown.min_value,
+            60,
+            0.0, 1.0
+        )
 
 func _on_health_ticker_timeout() -> void:
     if head.health < head.health_max:
