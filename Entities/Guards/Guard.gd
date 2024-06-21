b5213c1201e6cf118f009be322b3982750ce4ff6
nnda <96333146+nndda@users.noreply.github.com>
2024-06-21 15:28:04 +0800

    Refactor base entity
    Exports stats & configurations variables. Implement gameplay debuff/effects. Improve various minor functionalities.

--- Entities/Guards/Guard.gd
+++ Entities/Guards/Guard.gd
@@ -2,17 +2,34 @@ extends CharacterBody2D
 
 # Base scene for ALL entities
 
+@export_category("Stats")
+
 @export var health : float = 50.0
-@export var speed : float = 80.0
+@export_range(1.25, 17.50, 0.25) var speed : float = 10.0
+var speed_init : float
+
+@export_range(0.0, 1.0) var knockback_mult        : float = 1.0
+
+@export_range(0.0, 5.0, 0.1) var init_cooldown    : float = 0.0
+@export_range(0.8, 5.0, 0.1) var cooldown_min     : float = 0.8
+@export_range(0.8, 5.0, 0.1) var cooldown_max     : float = 0.8
+
+@onready var cooldown_timer : Timer = $AttackCooldown
+var cooldown_timer_left : float = 0.0
+
+@export_category("Configurations")
 
 ## Stop aiming to player if firing
 @export var freeze_on_fire := false
+
+@export var keep_player_distance : float = 190.0
 
 ## Is the entity moving away or approaching the player's head.
 @export var moving_away := false
 
 ## Is the entity able to move.
 @export var stationary := false
+@export var stationary_follow_rotate := false
 
 ## Is the entity immune to any damage (invincible).
 @export var immune := false
@@ -22,20 +39,23 @@ extends CharacterBody2D
 
 @export_range(0.0, 1.0)\
 var rotate_weight : float = 0.12
+var rotate_weight_init : float
 
 @export_group("Nodes")
 
 ## Designated 'patrol' path for non-stationary entity. Entity will not follow their path once triggered
-@export_node_path("PathFollow2D") var path_ : NodePath; var path : PathFollow2D
+@export var path : PathFollow2D
 
 ## Custom Area2D trigger that will detect player. Replaced the default TriggerArea node
-@export_node_path("Area2D") var custom_trigger_ : NodePath; var custom_trigger : Area2D
+@export var custom_trigger : Area2D
 
 ## Weapon scene for the entity
-@export_node_path("Node2D") var weapon_node : NodePath; var weapon : Node2D
+@export var weapon : Node2D
 
 ## The 'death scene' for when the entity is killed or destroyed
-@export_node_path("Node2D") var corpse_scene : NodePath; var corpse : Node2D
+@export var corpse : Node2D
+
+#@export var weapon_collision_exceptions : Array[CollisionObject2D]
 
 ## Particle scene for when the entity is attacked or damaged by the player
 @export_file("*.tscn") var blood_scene : String = "res://Shaders/Particles/BloodSplatters.tscn"; var blood : Node
@@ -45,17 +65,14 @@ var firing          := false
 signal triggered
 signal killed
 
-@export_group("Timing")
-@export_range(0.0, 5.0, 0.1) var init_cooldown    : float = 0.0
-@export_range(0.8, 5.0, 0.1) var cooldown         : float = 0.8
-@onready var cooldown_timer : Timer = $AttackCooldown
-
 var fire_clear  := false
 var fire_ready  := false
 var clear       := true
 var player_near := false
 
+@warning_ignore("unused_signal")
 signal path_changed(path)
+@warning_ignore("unused_signal")
 signal target_reached
 @onready var nav_agent : NavigationAgent2D = $NavigationAgent2D
 
@@ -66,42 +83,54 @@ var player_angle    : float
 @onready var sprite_base : Sprite2D = $Sprite2D
 @onready var visibility_handler_enabler : VisibleOnScreenEnabler2D = $VisibilityHandler/VisibleOnScreenEnabler2D
 
+func _enter_tree() -> void:
+    if !is_in_group(&"free"):
+        await weapon.tree_entered
+        weapon.entity_holder = self
+
 func _ready() -> void:
-    path = get_node_or_null(path_)
-    custom_trigger = get_node_or_null(custom_trigger_)
+    if !is_in_group(&"free"):
+        randomize()
+        rotate_weight_init = rotate_weight
+        speed_init = speed
 
-    corpse = get_node(corpse_scene)
-    corpse.reparent(Global.layer_dict[^"Objects/Corpses"])
+        corpse.reparent(Global.layer_dict[^"Objects/Corpses"])
 
-    corpse.modulate.a       = 0.8
-    corpse.z_as_relative    = false
-    corpse.z_index          = -100
+        corpse.modulate.a       = 0.8
+        corpse.z_as_relative    = false
+        corpse.z_index          = -100
 
-    weapon = get_node(weapon_node)
+        if corpse.get(&"blood_trail_trigger") != null:
+            corpse.blood_trail_trigger.monitoring = false
 
-    clear = init_cooldown == 0.0
-    cooldown_timer.wait_time = cooldown
+        clear = init_cooldown == 0.0
+        cooldown_timer.wait_time = randf_range(
+            cooldown_min, cooldown_max
+        )
 
-    if !manual_trigger:
-        if custom_trigger == null:
-            custom_trigger_ = ^"TriggerArea" # Set default
+        if !manual_trigger:
+            if custom_trigger != null:
+                custom_trigger.collision_mask = $TriggerArea.collision_mask
+                custom_trigger.body_entered.connect(_on_trigger_body_entered)
+                $TriggerArea.queue_free()
         else:
             $TriggerArea.queue_free()
-            custom_trigger.collision_mask = 512
-            custom_trigger.body_entered.connect(_on_trigger_body_entered)
-    else:
-        $TriggerArea.queue_free()
 
-    Global.camera_shaken_by_player.connect(trigger_if_near)
+        Global.camera_shaken_by_player.connect(trigger_if_near)
 
-    if stationary:
-        nav_agent = null
-        for free_itms : Node in [ $KeepDistance, $KeepMove, $NavigationAgent2D ]:
-            free_itms.queue_free()
+        if stationary:
+            nav_agent = null
+            for free_itms : Node in [ $KeepDistance, $KeepMove, $NavigationAgent2D ]:
+                free_itms.queue_free()
 
-    else:
-        for hidden_item : Node in [ $KeepDistance, $KeepMove ]:
-            hidden_item.visible = false
+        else:
+            for hidden_item : Node in [ $KeepDistance, $KeepMove ]:
+                hidden_item.visible = false
+
+        #for n in weapon_collision_exceptions:
+            #weapon.add_raycast_exception
+
+        visibility_handler_enabler.global_position = global_position
 
 func _process(_delta : float) -> void:
     corpse.global_position = global_position
@@ -123,28 +152,51 @@ func _process(_delta : float) -> void:
             global_rotation = path.global_rotation
 
     if stationary:
-        sprite_base.rotation_degrees = -rotation_degrees
+        if !stationary_follow_rotate:
+            sprite_base.rotation_degrees = -rotation_degrees
+    else:
+        visibility_handler_enabler.global_position = global_position
 
     if fire_clear:
+        if cooldown_timer.time_left <= 0.0:
+            cooldown_timer.start()
         weapon.fire()
         fire_ready = false
 
-    visibility_handler_enabler.global_position = global_position
+
+    cooldown_timer_left = cooldown_timer.time_left
+    lfire_coll = weapon.fire_function.line_of_fire.is_colliding()
+    lsight_coll = weapon.line_of_sight.is_colliding()
+    lobst_coll = weapon.obstacle_sight.is_colliding()
+    on_line = weapon.on_line
+    has_obstacle = weapon.has_obstacle
 
 # TODO:  trigger system is a mess
 func _physics_process(_delta : float) -> void:
     if is_triggered:
         if !stationary:
+            #nav_agent.set_velocity(Vector2(80.0, 80.0))
             nav_agent.set_velocity(Vector2(speed, speed))
 
+            #if weapon.has_obstacle or !weapon.player_on_sight:
+                #look_target = nav_agent.get_next_path_position()
+            #else:
+                #look_target = Global.head_pos
+
+#var look_target : Vector2
 func aim_to_player() -> void:
     player_angle = (Global.head_pos - global_position).angle()
     global_rotation = lerp_angle(global_rotation, player_angle, rotate_weight)
     rotation_degrees = wrapf(rotation_degrees, 0.0, 360.0)
 
+func _on_weapon_animation_started(anim : StringName) -> void:
+    if anim == &"Firing":
+        cooldown_timer.stop()
 func _on_weapon_animation_finished(anim : StringName) -> void:
     if anim == &"Firing":
-        cooldown_timer.start(cooldown)
+        cooldown_timer.start(randf_range(
+            cooldown_min, cooldown_max
+        ))
 func _on_attack_cooldown_timeout() -> void:
     fire_ready = true
 
@@ -153,21 +205,34 @@ func damage(power : float) -> void:
         triggered.emit()
 
     if !immune:
-        #print(self," damaged :", power)
         health -= power
 
         if health <= 0:
-            corpse.z_as_relative = true
-            corpse.z_index = 0
-            killed.emit()
-            queue_free()
+            kill()
 
-        else:
-            if power > 5.0:
-                blood = load(blood_scene).instantiate()
-                blood.custom_init_pos = true
-                blood.init_pos = global_position
-                Global.layer_dict[^"Objects/Particles"].add_child(blood)
+        if power > 0.0:
+            # blood particle
+            blood = load(blood_scene).instantiate()
+            blood.custom_init_pos = true
+            blood.init_pos = global_position
+            Global.layer_dict[^"Objects/Particles"].add_child(blood)
+
+            # knockback
+            effect_knockback(power * knockback_mult)
+
+var is_killed := false
+func kill() -> void:
+    if !is_killed:
+        is_killed = true
+        corpse.z_as_relative = true
+        corpse.z_index = 0
+        corpse.visible = true
+
+        if corpse.get(&"blood_trail_trigger") != null:
+            corpse.blood_trail_trigger.monitoring = true
+
+        killed.emit()
+        queue_free()
 
 # TODO:  navigation is a mess
 var arrived := false
@@ -178,7 +243,7 @@ func set_target_pos(pos : Vector2) -> void:
 var direction : Vector2
 func _on_navigation_velocity_computed(safe_velocity : Vector2) -> void:
     if !nav_agent.is_navigation_finished():
-        velocity = safe_velocity * direction
+        velocity = safe_velocity * direction * speed
         move_and_slide()
     elif !arrived:
         arrived = true
@@ -192,13 +257,13 @@ func _on_navigation_velocity_computed(safe_velocity : Vector2) -> void:
 @onready var keep_move_b : Node2D = $KeepMove/n2
 
 func _on_update_player_pos_timeout() -> void:
-    if global_position.distance_to(Global.head_pos) >= 160.0:
+    if global_position.distance_to(Global.head_pos) >= keep_player_distance:
         if !moving_away:
             direction = global_position.direction_to(Global.head_pos)
             set_target_pos(Global.head_pos)
     else:
         randomize()
-        keep_distance.rotation_degrees = randf_range(-15, 15)
+        keep_distance.rotation_degrees = randf_range(-15.0, 15.0)
         keep_distance_pos.position = Vector2(
             randf_range(keep_distance_a.position.x, keep_distance_b.position.x),
             randf_range(keep_move_a.position.y, keep_move_b.position.y)
@@ -206,8 +271,8 @@ func _on_update_player_pos_timeout() -> void:
         direction = global_position.direction_to(keep_distance_pos.global_position)
         set_target_pos(keep_distance_pos.global_position)
 
-func trigger_if_near() -> void:
-    if player_near:
+func trigger_if_near(substantial : bool) -> void:
+    if player_near and substantial:
         triggered.emit()
 
 func _on_trigger_body_entered(_body : Node2D) -> void:
@@ -221,8 +286,9 @@ func _on_triggered() -> void:
 
     if !is_triggered:
         is_triggered = true
-        cooldown_timer.start(cooldown)
-        printt("\t%s is triggered" % get_name())
+        cooldown_timer.start(randf_range(
+            cooldown_min, cooldown_max
+        ))
 
         if !manual_trigger:
             if $TriggerArea != null:
@@ -240,3 +306,33 @@ func _on_general_area_entered(area : Area2D) -> void:
 func _on_general_area_exited(area  : Area2D) -> void:
     if area.name == &"PlayerGeneralArea":
         player_near = false
+
+var shielded := false
+
+@onready var effect_dizzy_timer : Timer = $Effects/DizzyTimer
+func effect_dizzy(power : float):
+    rotate_weight = rotate_weight_init * randf_range(0.12, 0.21)
+    speed = speed_init * randf_range(0.12, 0.21)
+
+    effect_dizzy_timer.start(
+        randf_range(1.5, 3.5) + remap(
+            power, 50.0, 100.0, 2.0, 2.8
+        )
+    )
+    await effect_dizzy_timer.timeout
+
+    rotate_weight = rotate_weight_init
+    speed = speed_init
+
+@warning_ignore("unused_parameter")
+func effect_knockback(power : float):
+    move_and_collide(
+        remap(power, 0.0, 100.0, 0.0, -40.0) * global_position\
+        .direction_to(Global.player_physics_head.global_position)
+    )
+
+var lsight_coll := false
+var lfire_coll := false
+var lobst_coll := false
+var on_line := false
+var has_obstacle := false
