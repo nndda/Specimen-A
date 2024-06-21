819c251761a85345df2732bd4700320e2eb3bab8
nnda <96333146+nndda@users.noreply.github.com>
2024-06-21 15:32:02 +0800

    Improve `RayCast2D` detection
    Export used nodes directly instead of its `NodePath`. And from now on, only use `RayCast2D` for collision detection exclusively.

--- Entities/Weapons/Gun.gd
+++ Entities/Weapons/Gun.gd
@@ -3,67 +3,84 @@ extends Node2D
 ## Base script for ALL gun/projectile addon
 # NOTE : Require `FireFunction` Node with variable `animation_player`, containing animation named "Firing"
 
+@export var entity_holder : Node
+
 @export var damage_min : float = 0.85
 @export var damage_max : float = 1.9
 var firing := false
 
-@onready var fire_function := $FireFunction
-@onready var fire_function_anim : AnimationPlayer = $FireFunction.animation_player
+@export var fire_function : Node
+var fire_function_anim : AnimationPlayer
 
-@export_node_path("Area2D", "RayCast2D") var line_of_sight_; var line_of_sight : Node2D
-@export_node_path("Area2D", "RayCast2D") var obstacle_sight_; var obstacle_sight : Node2D
+@export var line_of_sight : RayCast2D
+@export var obstacle_sight : RayCast2D
+@export var friendly_sight : RayCast2D
 
-var parent : Node
-var trigger_area : NodePath
+var fire_paused := false
 var on_line := false
+var player_on_sight := false
+var has_obstacle := false
 
 const player_bodies : Array[StringName] = [&"Head", &"DamageCollision"]
 
-func set_on_line(object : Node2D) -> void:
-    if player_bodies.has(object.get_name()):
-        on_line = true
-func set_off_line(object : Node2D) -> void:
-    if !player_bodies.has(object.get_name()):
-        on_line = false
+#func add_raycast_exceptions(body : CollisionObject2D) -> void:
+    #line_of_sight.add_exception(body)
+    #obstacle_sight.add_exception(body)
+    #friendly_sight.add_exception(body)
 
-func _enter_tree() -> void:
-    parent = get_parent()
+func init_raycast_exceptions(raycast : RayCast2D, ignore_all : bool = true) -> void:
+    raycast.add_exception(Global.player_destroy_through)
+    raycast.add_exception(Global.player_general_area)
+
+    if ignore_all:
+        raycast.add_exception(Global.player_physics_head)
+        raycast.add_exception(Global.player_physics_body)
 
 func _on_line_of_fire_tree_entered() -> void:
     $LineOfSight/LineOfFire.enabled = false
 
 func _ready() -> void:
-    line_of_sight = get_node_or_null(line_of_sight_)
-    obstacle_sight = get_node_or_null(obstacle_sight_)
+    fire_function_anim = fire_function.animation_player
+    init_raycast_exceptions(line_of_sight, false)
+    init_raycast_exceptions(obstacle_sight)
+    init_raycast_exceptions(friendly_sight)
 
-    trigger_area =\
-        ^"../TriggerArea" if parent.custom_trigger == null else\
-        parent.custom_trigger_
-
-    if  line_of_sight != null:
-
-        if line_of_sight is RayCast2D:
-            line_of_sight.add_exception(get_node(trigger_area))
-            line_of_sight.add_exception(Global.player_destroy_through)
-
-        elif line_of_sight is Area2D:
-
-            line_of_sight.body_entered.connect(set_on_line)
-            line_of_sight.body_exited.connect(set_off_line)
-
-            line_of_sight.area_entered.connect(set_on_line)
-            line_of_sight.area_exited.connect(set_off_line)
-
-    fire_function_anim.animation_finished.connect(parent._on_weapon_animation_finished)
+    fire_function_anim.animation_started.connect(
+        entity_holder._on_weapon_animation_started
+    )
+    fire_function_anim.animation_finished.connect(
+        entity_holder._on_weapon_animation_finished
+    )
 
 func _process(_delta : float) -> void:
-    firing = fire_function_anim.current_animation == &"Firing" && fire_function_anim.is_playing()
-    parent.firing = firing
+    firing = fire_function_anim.current_animation == &"Firing"
+    entity_holder.firing = firing
 
 func _physics_process(_delta : float) -> void:
-    if parent.triggered:
-        if line_of_sight is RayCast2D:
-            on_line = line_of_sight.is_colliding() and !obstacle_sight.is_colliding()
+    if entity_holder.triggered:
+        player_on_sight = line_of_sight.is_colliding()
+
+        # there has to be a better way
+        if !obstacle_sight.is_colliding():
+            has_obstacle = false
+            if !firing and fire_paused:
+                fire_paused = false
+                entity_holder.fire_ready = true
+                entity_holder.cooldown_timer.start()
+        else:
+            has_obstacle =\
+                line_of_sight.global_position.distance_to(
+                    line_of_sight.get_collision_point()
+                ) >\
+                line_of_sight.global_position.distance_to(
+                    obstacle_sight.get_collision_point()
+                )
+            if has_obstacle:
+                if firing and !fire_paused:
+                    fire_paused = true
+                    fire_function.fire_stop()
+
+        on_line = player_on_sight and !has_obstacle
 
 func fire() -> void:
     fire_function_anim.play(&"Firing")
