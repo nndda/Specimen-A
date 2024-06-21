06f9f755e83769ce4a8142524cd0feef51e2907c
nnda <96333146+nndda@users.noreply.github.com>
2024-06-22 00:39:31 +0800

    Flesh out main menu

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -9,7 +9,13 @@ extends Node2D
 
 @export_category("Pop Up Windows")
 @export var config_menu : Control
-@export var exit_confirm : AcceptDialog
+@export var exit_confirm : ConfirmationDialog
+@export var goto_url_confirm : ConfirmationDialog
+var goto_url_dest : String
+
+@export_category("Bottom Menu")
+@export var version_label : Label
+@export var source_code_button : Button
 
 func _ready() -> void:
     Global.current_scene = self
@@ -20,17 +26,35 @@ func _ready() -> void:
     config_menu.sync_config()
 
     Audio.set_dialogue_window(exit_confirm)
+    Audio.set_dialogue_window(goto_url_confirm)
     Audio.connect_audio()
+    
+    version_label.text = "v%s" % ProjectSettings\
+        .get_setting("application/config/version")
+
+func _on_start_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_config_pressed() -> void:
+    config_menu.visible = true
+
+func _on_credits_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_achievements_pressed() -> void:
+    pass # Replace with function body.
 
 func _on_exit_pressed() -> void:
     exit_confirm.popup_centered()
 
-func _config_pressed() -> void:
-    config_menu.visible = true
-
 func _on_exit_confirmation_dialog_confirmed() -> void:
     get_tree().quit()
 
+func goto_url(url : String) -> void:
+    goto_url_dest = url
+    url = url.trim_prefix("https://")
+    goto_url_confirm.dialog_text = "Go to '%s'?" % url
+    goto_url_confirm.popup_centered()
 
-
-
+func _on_goto_url_confirmed() -> void:
+    print(goto_url_dest)
