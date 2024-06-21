1f8d7c0c8d81188d996b109f427b3cd1bb3c78cd
nnda <96333146+nndda@users.noreply.github.com>
2024-06-21 15:33:51 +0800

    Update to match the base weapon script

--- Entities/Weapons/Gun-Rifle.gd
+++ Entities/Weapons/Gun-Rifle.gd
@@ -1,38 +1,56 @@
 extends Node
 
-@export_node_path("AnimationPlayer") var animation_player_ : NodePath
-@onready var animation_player : AnimationPlayer = get_node(animation_player_)
+@export var spread_degrees : float = 1.98
+@export var animation_player : AnimationPlayer
 
-@onready var wielder := $"../.."
-@onready var weapon := $".."
+var entity_holder : Node
+@onready var weapon : Node2D = $".."
 
-@onready var line_of_sight := $"../LineOfSight"
-@onready var line_of_fire := $"../LineOfSight/LineOfFire"
-@onready var line_of_fire_const := $"../LineOfSight/LineOfFire/Const"
+@export_category("Sights")
+@export var line_of_sight : RayCast2D
+@export var line_of_fire : RayCast2D
+@export var line_of_fire_const : RayCast2D
 var line_of_fire_collider : Object
 
-@onready var bullet_path := $"../BulletPath"
-@onready var bullet_path_default_pos := $"../CollidingPointDefault"
-@onready var bullet_spark := $"../BulletSpark"
+@export_category("Visuals")
+@export var bullet_path : Line2D
+@export var bullet_path_default_pos : Marker2D
+@export var bullet_spark : GPUParticles2D
 
-@onready var colliding_point := $"../CollidingPoint"
+@export var colliding_point : Marker2D
 
 var hit := false
 
 func _ready() -> void:
+    entity_holder = weapon.entity_holder
     animation_player.animation_started.connect(firing_started)
     animation_player.animation_finished.connect(firing_ended)
 
 func fire() -> void:
     randomize()
     Camera.shake_start(5.0, 0.09, 32.0)
-    line_of_fire.rotation_degrees = randf_range(-2.25, 2.25)
+    line_of_fire.rotation_degrees = randf_range(-spread_degrees, spread_degrees)
     bullet_path.rotation_degrees = line_of_fire.rotation_degrees
+
+func fire_stop() -> void:
+    animation_player.stop()
+    animation_player.play(&"RESET")
+
+    line_of_fire.enabled = false
+    line_of_fire_const.enabled = false
+
+    bullet_spark.emitting = false
+    bullet_spark.visible = false
+    bullet_path.visible = false
+    bullet_path.points[1] = bullet_path_default_pos.position
 
 func firing_started(anim : StringName) -> void:
     if anim == &"Firing":
-        bullet_path.visible = true
+        line_of_fire.enabled = true
         line_of_fire_const.enabled = true
+
+        bullet_spark.visible = true
+        bullet_path.visible = true
 
 func firing_ended(anim : StringName) -> void:
     if anim == &"Firing":
@@ -41,10 +59,10 @@ func firing_ended(anim : StringName) -> void:
         bullet_path.points[1] = bullet_path_default_pos.position
 
 func _physics_process(_delta : float) -> void:
-    if wielder.triggered:
+    if entity_holder.triggered:
             hit = line_of_fire.is_colliding()
             bullet_path.points[0] = Vector2.ZERO
-            bullet_spark.visible = hit
+            bullet_spark.emitting = hit
 
             if hit:
                 colliding_point.global_position = line_of_fire_const.get_collision_point()
@@ -53,7 +71,9 @@ func _physics_process(_delta : float) -> void:
 
                 bullet_spark.global_position = colliding_point.global_position
                 if line_of_fire_collider != null:
+                    var damage_output : float = randf_range(weapon.damage_min, weapon.damage_max)
+
                     if line_of_fire_collider.has_method(&"damage_player"):
-                        line_of_fire_collider.damage_player(randf_range(
-                                weapon.damage_min,
-                                weapon.damage_max))
+                        line_of_fire_collider.damage_player(damage_output)
+                    elif line_of_fire_collider.has_method(&"damage_prop"):
+                        line_of_fire_collider.damage_prop(damage_output)
