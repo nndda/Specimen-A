791ad16877192a36ecb35ecf149a4367dd59ad16
nnda <96333146+nndda@users.noreply.github.com>
2024-09-01 06:21:04 +0800

    Cache light node

--- Worlds/Objects/Cable.gd
+++ Worlds/Objects/Cable.gd
@@ -1,16 +1,17 @@
 extends Line2D
 
 @export var lights := false
+@onready var lights_node : Line2D = $Light
 
 func _ready() -> void:
     if !lights:
-        $Light.queue_free()
+        lights_node.queue_free()
         set_script(null)
     else:
-        $Light.points = points
+        lights_node.points = points
 
 func lights_off() -> void:
     if lights:
         lights = false
-        $Light.queue_free()
+        lights_node.queue_free()
         set_script(null)
