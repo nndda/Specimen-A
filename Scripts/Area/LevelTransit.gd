60ca076175cde077ddd6c2efb6a436da8dc06be0
nnda <96333146+nndda@users.noreply.github.com>
2024-09-01 22:13:38 +0800

    Handle empty scene

--- Scripts/Area/LevelTransit.gd
+++ Scripts/Area/LevelTransit.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 @export_file("*.tscn", "*.scn") var level_transition_scene : String
 
+func _ready() -> void:
+    if level_transition_scene.is_empty():
+        push_error("Empty level transition.")
+
 func _on_body_entered(body : Node2D) -> void:
     if body.name == Global.PLAYER_HEAD_NAME:
         (Global.player.ui as CanvasLayer).visible = false
