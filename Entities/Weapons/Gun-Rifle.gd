1526ea9a9727e79b584096164402e79ff2e5052f
nnda <96333146+nndda@users.noreply.github.com>
2024-09-01 19:12:59 +0800

    Only process physics interaction when firing

--- Entities/Weapons/Gun-Rifle.gd
+++ Entities/Weapons/Gun-Rifle.gd
@@ -16,10 +16,13 @@ var line_of_fire_collider : Object
 
 @export var colliding_point : Marker2D
 
+var firing := false
+
 var damage_min : float
 var damage_max : float
 
 var hit := false
+var damage_output : float
 
 func _ready() -> void:
     animation_player.animation_started.connect(firing_started)
@@ -56,22 +59,25 @@ func firing_ended(anim : StringName) -> void:
         line_of_fire_const.enabled = false
         bullet_path.points[1] = bullet_path_default_pos.position
 
-var damage_output : float
+func _process(_delta: float) -> void:
+    firing = animation_player.current_animation == &"Firing"
+
 func _physics_process(_delta : float) -> void:
-    hit = line_of_fire.is_colliding()
-    bullet_path.points[0] = Vector2.ZERO
-    bullet_spark.emitting = hit
+    if firing:
+        hit = line_of_fire.is_colliding()
+        bullet_path.points[0] = Vector2.ZERO
+        bullet_spark.emitting = hit
 
-    if hit:
-        colliding_point.global_position = line_of_fire_const.get_collision_point()
-        bullet_path.points[1] = colliding_point.position
-        line_of_fire_collider = line_of_fire.get_collider()
+        if hit:
+            colliding_point.global_position = line_of_fire_const.get_collision_point()
+            bullet_path.points[1] = colliding_point.position
+            line_of_fire_collider = line_of_fire.get_collider()
 
-        bullet_spark.global_position = colliding_point.global_position
-        if line_of_fire_collider != null:
-            damage_output = randf_range(damage_min, damage_max)
+            bullet_spark.global_position = colliding_point.global_position
+            if line_of_fire_collider != null:
+                damage_output = randf_range(damage_min, damage_max)
 
-            if line_of_fire_collider.has_method(&"damage_player"):
-                line_of_fire_collider.damage_player(damage_output)
-            elif line_of_fire_collider.has_method(&"damage_prop"):
-                line_of_fire_collider.damage_prop(damage_output)
+                if line_of_fire_collider.has_method(&"damage_player"):
+                    line_of_fire_collider.damage_player(damage_output)
+                elif line_of_fire_collider.has_method(&"damage_prop"):
+                    line_of_fire_collider.damage_prop(damage_output)
