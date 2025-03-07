c4799afa75a219e1e8cea33d679591fa2c9eb05f
nnda <96333146+nndda@users.noreply.github.com>
2025-03-07 10:22:12 +0700

    Improve `Mine` object & resolve memory leak

--- Entities/Weapons/Mine.gd
+++ Entities/Weapons/Mine.gd
@@ -7,9 +7,6 @@ signal triggered
 
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 @onready var explosion_particles : GPUParticles2D = $Explosion
-@onready var shards : CPUParticles2D = preload(
-    "res://Shaders/Particles/GlassShardsSparks.tscn"
-).instantiate()
 
 func trigger() -> void:
     animation_player.play(&"RESET")
@@ -18,22 +15,31 @@ func trigger() -> void:
 
     $AnimationPlayer.play(&"Detonate")
     triggered.emit()
-    add_child(shards)
 
     for n in get_overlapping_bodies():
         if n.is_in_group(&"entity"):
             n.kill()
 
+    var shards : ParticlesHandler = (
+        load("res://Shaders/Particles/GlassShardsSparks.tscn") as PackedScene
+    ).instantiate()
+
+    shards.amount = 12
+    shards.init_pos = global_position
+
+    add_child(shards)
+
 func _ready() -> void:
     animation_player.stop()
+    var anim_offset_timer : Timer = $AnimationPlayer/OffsetTimer
 
-    $AnimationPlayer/OffsetTimer.timeout.connect(
+    anim_offset_timer.timeout.connect(
         animation_player.play.bind(&"Idle")
     )
-    $AnimationPlayer/OffsetTimer.timeout.connect(
-        $AnimationPlayer/OffsetTimer.queue_free, CONNECT_DEFERRED
+    anim_offset_timer.timeout.connect(
+        anim_offset_timer.queue_free, CONNECT_DEFERRED
     )
-    $AnimationPlayer/OffsetTimer.start(randf_range(0.7, 2.1))
+    anim_offset_timer.start(randf_range(0.7, 2.1))
 
 func _on_body_entered(body : Node2D) -> void:
     if body.name == Global.PLAYER_HEAD_NAME:
@@ -41,17 +47,17 @@ func _on_body_entered(body : Node2D) -> void:
         body_entered.disconnect(_on_body_entered)
         body.damage_player(randf_range(damage_min, damage_max))
 
-func _on_animation_finished(anim_name : StringName) -> void:
+func _on_animation_finished(anim_name : StringName) -> void:    
+    animation_player = null
+    explosion_particles = null
+
     if anim_name == &"Detonate":
         for n : Node in [
             $Sprite2D,
             $IdleLights,
             $ExplosionLight,
-            animation_player,
+            $AnimationPlayer,
         ]:
             n.queue_free()
 
-    animation_player = null
-    explosion_particles = null
-    shards = null
     set_script(null)
